634f678a60dee2424cf03e9730d4d2284f72074a
Gary Dusbabek <gdusbabek@gmail.com>
2013-08-06 08:04:04 -0700

    fix bad import

--- blueflood-core/src/main/java/com/rackspacecloud/blueflood/outputs/Serializers/OutputSerializer.java
+++
@@ -1,7 +0,0 @@
-package com.rackspacecloud.blueflood.outputs.serializers;
-
-import com.rackspacecloud.blueflood.outputs.formats.MetricData;
-
-public interface OutputSerializer<T> {
-    public T transformRollupData(MetricData metricData);
-}
