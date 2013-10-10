06a3befcbe955902f6e0d95c54c348a4d8db2e28
gdusbabek <gdusbabek@gmail.com>
2013-10-10 13:02:26 -0500

    Demote `Rollup.java` to an interface.

--- blueflood-core/src/main/java/com/rackspacecloud/blueflood/types/TimerRollup.java
+++ blueflood-core/src/main/java/com/rackspacecloud/blueflood/types/TimerRollup.java
@@ -14,7 +14,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public class TimerRollup extends Rollup implements IBasicRollup {
+public class TimerRollup implements Rollup, IBasicRollup {
     private long sum = 0;
     private long count = 0;
     private double count_ps = 0;
