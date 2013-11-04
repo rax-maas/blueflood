744df70b9dab2b4cb7339f5f06af2ba77c1edd8c
gdusbabek <gdusbabek@gmail.com>
2013-11-04 13:12:22 -0600

    instrument the stat parser.

--- blueflood-statsd/src/main/java/com/rackspacecloud/blueflood/statsd/StatParser.java
+++ blueflood-statsd/src/main/java/com/rackspacecloud/blueflood/statsd/StatParser.java
@@ -5,12 +5,15 @@ import com.rackspacecloud.blueflood.concurrent.AsyncFunctionWithThreadPool;
 import com.rackspacecloud.blueflood.statsd.containers.Conversions;
 import com.rackspacecloud.blueflood.statsd.containers.StatsCollection;
 import com.rackspacecloud.blueflood.statsd.containers.Stat;
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.Meter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Stats sent to graphite from statsd come in this format:
@@ -18,6 +21,8 @@ import java.util.concurrent.ThreadPoolExecutor;
  */
 public class StatParser extends AsyncFunctionWithThreadPool<Collection<CharSequence>, StatsCollection> {
     private static Logger log = LoggerFactory.getLogger(StatParser.class);
+    private static Meter invalidStatsMeter = Metrics.newMeter(StatParser.class, "Invalid Stats", "Ingestion", TimeUnit.MINUTES);
+    private static Meter statParseErrors = Metrics.newMeter(StatParser.class, "Stat Parse Errors", "Ingestion", TimeUnit.MINUTES);
     
     public StatParser(ThreadPoolExecutor executor) {
         super(executor);
@@ -36,9 +41,11 @@ public class StatParser extends AsyncFunctionWithThreadPool<Collection<CharSeque
                             stats.add(stat);
                         } else {
                             log.debug("Invalid stat line: {}", seq.toString());
+                            invalidStatsMeter.mark();
                         }
                     } catch (Throwable nastyParseBug) {
                         log.error(nastyParseBug.getMessage(), nastyParseBug);
+                        statParseErrors.mark();
                     }
                 }
                 return stats;
