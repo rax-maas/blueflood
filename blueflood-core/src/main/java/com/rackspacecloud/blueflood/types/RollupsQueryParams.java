ab4771219c9f41f1a703797590b27fdd7bcf7c13
Lakshmi Kannan <lakshmi.kannan@rackspace.com>
2013-11-21 10:57:59 -0800

    Refactor plot request parsing logic into its own class

---
+++ blueflood-core/src/main/java/com/rackspacecloud/blueflood/types/RollupsQueryParams.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2013 Rackspace
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.rackspacecloud.blueflood.types;
+
+import com.rackspacecloud.blueflood.outputs.serializers.OutputSerializer;
+import com.rackspacecloud.blueflood.rollup.Granularity;
+
+import java.util.Set;
+
+public class RollupsQueryParams {
+    private int points;
+    private Resolution resolution;
+    private final Range range;
+    private final Set<OutputSerializer.MetricStat> stats;
+    private boolean isPoints = false;
+
+    private RollupsQueryParams(long from, long to, Set<OutputSerializer.MetricStat> stats) {
+        if (from >= to) {
+            throw new IllegalArgumentException("'from' timestamp has to be strictly less than 'to'.");
+        }
+        this.stats = stats;
+        this.range = new Range(from, to);
+        this.points = 0;
+        this.resolution = Resolution.FULL;
+    }
+
+    public RollupsQueryParams(long from, long to, int points, Set<OutputSerializer.MetricStat> stats) {
+        this(from, to, stats);
+        this.isPoints = true;
+        this.points = points;
+    }
+
+    public RollupsQueryParams(long from, long to, Resolution resolution, Set<OutputSerializer.MetricStat> stats) {
+        this(from, to, stats);
+        this.resolution = resolution;
+        this.isPoints = false;
+    }
+
+    public boolean isGetByPoints() {
+        return isPoints;
+    }
+
+    public boolean isGetByResolution() {
+        return !isPoints;
+    }
+
+    public Granularity getGranularity() {
+        if (isPoints) {
+            return Granularity.granularityFromPointsInInterval(range.getStart(), range.getStop(), points);
+        } else {
+            return Granularity.granularities()[resolution.getValue()];
+        }
+    }
+
+    public Range getRange() {
+        return range;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public Resolution getResolution() {
+        return resolution;
+    }
+
+    public Set<OutputSerializer.MetricStat> getStats() {
+        return stats;
+    }
+}
---
+++ blueflood-http/src/main/java/com/rackspacecloud/blueflood/outputs/utils/PlotRequestParser.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2013 Rackspace
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.rackspacecloud.blueflood.outputs.utils;
+
+import com.rackspacecloud.blueflood.exceptions.InvalidRequestException;
+import com.rackspacecloud.blueflood.outputs.serializers.OutputSerializer;
+import com.rackspacecloud.blueflood.types.Resolution;
+import com.rackspacecloud.blueflood.types.RollupsQueryParams;
+
+import java.util.*;
+
+public class PlotRequestParser {
+    private static final Set<OutputSerializer.MetricStat> defaultStats;
+
+    static {
+        defaultStats = new HashSet<OutputSerializer.MetricStat>();
+        defaultStats.add(OutputSerializer.MetricStat.AVERAGE);
+        defaultStats.add(OutputSerializer.MetricStat.NUM_POINTS);
+    }
+
+    public static RollupsQueryParams parseParams(Map<String, List<String>> params) throws InvalidRequestException {
+        if (params == null || params.isEmpty()) {
+            throw new InvalidRequestException("No query parameters present.");
+        }
+
+        List<String> points = params.get("points");
+        List<String> res = params.get("resolution");
+        List<String> from = params.get("from");
+        List<String> to = params.get("to");
+        List<String> select = params.get("select");
+
+        if (points == null && res == null) {
+            throw new InvalidRequestException("Either 'points' or 'resolution' is required.");
+        }
+
+        if (points != null && points.size() != 1) {
+            throw new InvalidRequestException("Invalid parameter: points=" + points);
+        } else if (res != null && res.size() != 1) {
+            throw new InvalidRequestException("Invalid parameter: resolution=" + res);
+        } else if (from == null || from.size() != 1) {
+            throw new InvalidRequestException("Invalid parameter: from=" + from);
+        } else if (to == null || to.size() != 1) {
+            throw new InvalidRequestException("Invalid parameter: to="+ to);
+        }
+
+        long fromTime = Long.parseLong(from.get(0));
+        long toTime = Long.parseLong(to.get(0));
+
+        if (toTime <= fromTime) {
+            throw new InvalidRequestException("paramter 'to' must be greater than 'from'");
+        }
+
+        Set<OutputSerializer.MetricStat> stats = getStatsToFilter(select);
+
+        if (points != null) {
+            return new RollupsQueryParams(fromTime, toTime, Integer.parseInt(points.get(0)), stats);
+        } else {
+            return new RollupsQueryParams(fromTime, toTime, Resolution.fromString(res.get(0)), stats);
+        }
+    }
+
+    public static Set<OutputSerializer.MetricStat> getStatsToFilter(List<String> select) {
+        if (select == null || select.isEmpty()) {
+            return defaultStats;
+        } else {
+            Set<OutputSerializer.MetricStat> filters = new HashSet<OutputSerializer.MetricStat>();
+            // handle case when someone does select=average,min instead of select=average&select=min
+            for (String stat : select) {
+                if (stat.contains(",")) {
+                    List<String> nestedStats = Arrays.asList(stat.split(","));
+                    filters.addAll(OutputSerializer.MetricStat.fromStringList(nestedStats));
+                } else {
+                    filters.add(OutputSerializer.MetricStat.fromString(stat));
+                }
+            }
+            return filters;
+        }
+    }
+}
