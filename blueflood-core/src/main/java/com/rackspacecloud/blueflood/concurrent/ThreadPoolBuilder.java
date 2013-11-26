f445a3e83a3101381e00539c47da729bd3e90902
Gary Dusbabek <gdusbabek@gmail.com>
2013-11-26 06:36:15 -0800

    fix merge conflict in ThreadPoolBuilder without borking whitepsace

--- blueflood-core/src/main/java/com/rackspacecloud/blueflood/concurrent/ThreadPoolBuilder.java
+++ blueflood-core/src/main/java/com/rackspacecloud/blueflood/concurrent/ThreadPoolBuilder.java
@@ -13,23 +13,22 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
- 
+
 package com.rackspacecloud.blueflood.concurrent;
- 
+
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
-import com.rackspacecloud.blueflood.utils.TimeValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
- 
+
 import java.util.concurrent.*;
- 
+
 public class ThreadPoolBuilder {
     private static final Logger log = LoggerFactory.getLogger(ThreadPoolBuilder.class);
- 
+
     private int corePoolSize = 10;
     private int maxPoolSize = 10;
     private int queueSize = 0;
-     
+    
     private RejectedExecutionHandler rejectedHandler = new ThreadPoolExecutor.CallerRunsPolicy();
     private Thread.UncaughtExceptionHandler exceptionHandler = new Thread.UncaughtExceptionHandler() {
         public void uncaughtException(Thread t, Throwable e) {
@@ -37,42 +36,36 @@ public class ThreadPoolBuilder {
         }
     };
     private String name = "Thread";
-    private TimeValue keepAliveTime = new TimeValue(30, TimeUnit.SECONDS);
- 
+
     public ThreadPoolBuilder() {
- 
+
     }
- 
+
     public ThreadPoolBuilder withCorePoolSize(int size) {
         this.corePoolSize = size;
         return this;
     }
- 
+
     public ThreadPoolBuilder withMaxPoolSize(int size) {
         this.maxPoolSize = size;
         return this;
     }
- 
+
     public ThreadPoolBuilder withUnboundedQueue() {
         this.queueSize = 0;
         return this;
     }
- 
+
     public ThreadPoolBuilder withBoundedQueue(int size) {
         this.queueSize = size;
         return this;
     }
-     
-    public ThreadPoolBuilder withKeepAliveTime(TimeValue time) {
-        this.keepAliveTime = time;
-        return this;
-    }
- 
+
     public ThreadPoolBuilder withRejectedHandler(RejectedExecutionHandler rejectedHandler) {
         this.rejectedHandler = rejectedHandler;
         return this;
     }
- 
+
     public ThreadPoolBuilder withName(String name) {
         // ensure we've got a spot to put the thread id.
         if (name.indexOf("%d") < 0) {
@@ -80,25 +73,24 @@ public class ThreadPoolBuilder {
         }
         this.name = name;
         return this;
- 
+
     }
- 
+
     public ThreadPoolBuilder withExeptionHandler(Thread.UncaughtExceptionHandler exceptionHandler) {
         this.exceptionHandler = exceptionHandler;
         return this;
     }
- 
+
     public ThreadPoolExecutor build() {
         String metricName = name.subSequence(0, name.length() - 3) + " work queue size"; // don't need the '-%d'
-        BlockingQueue<Runnable> workQueue = this.queueSize > 0 ? new ArrayBlockingQueue<Runnable>(queueSize) :
+        final BlockingQueue<Runnable> workQueue = this.queueSize > 0 ? new ArrayBlockingQueue<Runnable>(queueSize) :
                     new LinkedBlockingQueue<Runnable>();
-         
+        
         return new InstrumentedThreadPoolExecutor(
                 metricName,
                 corePoolSize,
                 maxPoolSize,
-                keepAliveTime.getValue(),
-                keepAliveTime.getUnit(),
+                30, TimeUnit.SECONDS, // hard code the timeout.
                 workQueue,
                 new ThreadFactoryBuilder().setNameFormat(name).setPriority(Thread.NORM_PRIORITY).setUncaughtExceptionHandler(exceptionHandler).build(),
                 rejectedHandler);
