a01bae004ddfbfb9feb48442207936692815a631
gdusbabek <gdusbabek@gmail.com>
2013-12-10 15:53:34 -0600

    yammer metrics -> codahale metrics.

--- blueflood-statsd/src/main/java/com/rackspacecloud/blueflood/statsd/StatParser.java
+++ blueflood-statsd/src/main/java/com/rackspacecloud/blueflood/statsd/StatParser.java
@@ -1,19 +1,18 @@
 package com.rackspacecloud.blueflood.statsd;
 
+import com.codahale.metrics.Meter;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.rackspacecloud.blueflood.concurrent.AsyncFunctionWithThreadPool;
 import com.rackspacecloud.blueflood.statsd.containers.Conversions;
 import com.rackspacecloud.blueflood.statsd.containers.StatCollection;
 import com.rackspacecloud.blueflood.statsd.containers.Stat;
-import com.yammer.metrics.Metrics;
-import com.yammer.metrics.core.Meter;
+import com.rackspacecloud.blueflood.utils.Metrics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Stats sent to graphite from statsd come in this format:
@@ -21,8 +20,8 @@ import java.util.concurrent.TimeUnit;
  */
 public class StatParser extends AsyncFunctionWithThreadPool<Collection<CharSequence>, StatCollection> {
     private static Logger log = LoggerFactory.getLogger(StatParser.class);
-    private static Meter invalidStatsMeter = Metrics.newMeter(StatParser.class, "Invalid Stats", "Ingestion", TimeUnit.MINUTES);
-    private static Meter statParseErrors = Metrics.newMeter(StatParser.class, "Stat Parse Errors", "Ingestion", TimeUnit.MINUTES);
+    private static Meter invalidStatsMeter = Metrics.meter(StatParser.class, "Stat Parser Invalid Stats");
+    private static Meter statParseErrors = Metrics.meter(StatParser.class, "Stat Parse Errors");
     
     public StatParser(ThreadPoolExecutor executor) {
         super(executor);
