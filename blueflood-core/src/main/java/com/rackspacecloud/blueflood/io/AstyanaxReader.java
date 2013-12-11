20e27b3ba16d6584bea0114e22ec608bd948e209
Lakshmi Kannan <lakshmi.kannan@rackspace.com>
2013-12-11 14:41:17 -0800

    Get rid of getBasicRollupDataToRoll

--- blueflood-core/src/main/java/com/rackspacecloud/blueflood/io/AstyanaxReader.java
+++ blueflood-core/src/main/java/com/rackspacecloud/blueflood/io/AstyanaxReader.java
@@ -86,34 +86,6 @@ public class AstyanaxReader extends AstyanaxIO {
         }
     }
 
-    /**
-     * reads a column slice and returns data points. this method doesn't do any checking to ensure that the range
-     * specified is over a single slot.
-     * @param locator
-     * @param range
-     * @param srcCF
-     * @return
-     */
-    public Points<BasicRollup> getBasicRollupDataToRoll(Locator locator, Range range, ColumnFamily<Locator, Long> srcCF) throws IOException {
-        if (srcCF.equals(AstyanaxIO.CF_METRICS_FULL))
-            throw new IOException("This method should not be used for full resolution data");
-        
-        AbstractSerializer serializer = NumericSerializer.serializerFor(BasicRollup.class);
-        ColumnList<Long> cols = getNumericRollups(locator, srcCF, range.start, range.stop);
-        Points<BasicRollup> points = new Points<BasicRollup>();
-        
-        try {
-            for (Column<Long> col : cols) {
-                points.add(new Points.Point<BasicRollup>(col.getName(), (BasicRollup) col.getValue(serializer)));
-            }
-        } catch (RuntimeException ex) {
-            log.error("Problem deserializing data", ex);
-            throw new IOException(ex);
-        }
-
-        return points;    
-    }
-    
     public Points<SimpleNumber> getSimpleDataToRoll(Locator locator, Range range) throws IOException {
         AbstractSerializer<SimpleNumber> serializer = NumericSerializer.serializerFor(SimpleNumber.class);
         ColumnList<Long> cols = getNumericRollups(locator, AstyanaxIO.CF_METRICS_FULL, range.start, range.stop);
