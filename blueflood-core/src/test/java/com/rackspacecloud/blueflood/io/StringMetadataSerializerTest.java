1092e62c15f39c0dfba3d10aeca7ea5e1a263ccf
James Burkhart <james@jamesburkhart.com>
2014-01-07 16:02:15 -0800

    remove references to metadataserializer serializing non-string values

--- blueflood-core/src/test/java/com/rackspacecloud/blueflood/io/StringMetadataSerializerTest.java
+++ blueflood-core/src/test/java/com/rackspacecloud/blueflood/io/StringMetadataSerializerTest.java
@@ -21,108 +21,25 @@ import org.junit.Test;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class StringMetadataSerializerTest {
 
-    // TODO: uncomment most of these, pending https://issues.rax.io/browse/CMD-139
-    
-//    @Test
-//    public void testLong() throws IOException {
-//        Object[] values = {
-//            Long.MIN_VALUE,
-//            Long.MIN_VALUE / 2,
-//            -1000L,
-//            -1L,
-//            0L,
-//            1L,
-//            1000L,
-//            Long.MAX_VALUE / 2,
-//            Long.MAX_VALUE
-//        };
-//        testRoundTrip(values);
-//    }
-//
-//    @Test
-//    public void testInteger() throws IOException {
-//        Object[] values = {
-//            Integer.MIN_VALUE,
-//            Integer.MIN_VALUE / 2,
-//            -1000,
-//            -1,
-//            0,
-//            1,
-//            1000,
-//            Integer.MAX_VALUE / 2,
-//            Integer.MAX_VALUE
-//        };
-//        testRoundTrip(values);
-//    }
-//
-//    @Test
-//    public void testFloat() throws IOException {
-//        Object[] values = {
-//            Float.MIN_VALUE,
-//            Float.MIN_VALUE / 2,
-//            -1000.0f,
-//            -1f,
-//            0f,
-//            1f,
-//            1000f,
-//            Float.MAX_VALUE / 2,
-//            Float.MAX_VALUE
-//        };
-//        testRoundTrip(values);
-//    }
-//
-//    @Test
-//    public void testDouble() throws IOException {
-//        Object[] values = {
-//            Double.MIN_VALUE,
-//            Double.MIN_VALUE / 2,
-//            -1000.0f,
-//            -1f,
-//            0f,
-//            1f,
-//            1000f,
-//            Double.MAX_VALUE / 2,
-//            Double.MAX_VALUE
-//        };
-//        testRoundTrip(values);
-//    }
-    
     @Test
     public void testString() throws IOException {
-        Object[] values = {
+        String[] values = {
             "abcdefg",
             "\u1234 \u0086 \uabcd \u5432",
             "ĆĐÈ¿ΔΞ€"
         };
         testRoundTrip(values);
     }
-    
-//    @Test
-//    public void testBinary() throws IOException {
-//        Random rand = new Random(System.currentTimeMillis());
-//        byte[][] values = new byte[6][];
-//        for (int i = 0; i < values.length; i++) {
-//            int target = 1 * (int)Math.pow(10, i);
-//            values[i] = new byte[target];
-//            rand.nextBytes(values[i]);
-//        }
-//        testRoundTrip(values);
-//    }
-    
-    private void testRoundTrip(Object[] objects) throws IOException {
-        for (Object obj : objects) {
-            byte[] buf = StringMetadataSerializer.get().toByteBuffer(obj).array();
+
+    private void testRoundTrip(String... strings) throws IOException {
+        for (String str : strings) {
+            byte[] buf = StringMetadataSerializer.get().toByteBuffer(str).array();
             ByteBuffer bb = ByteBuffer.wrap(buf);
-            if (obj instanceof byte[]) {
-                assertArrayEquals((byte[])obj, (byte[]) StringMetadataSerializer.get().fromByteBuffer(bb));
-            } else {
-                assertEquals(obj, StringMetadataSerializer.get().fromByteBuffer(bb));
-            }
+                assertEquals(str, StringMetadataSerializer.get().fromByteBuffer(bb));
         }
     }
 }
