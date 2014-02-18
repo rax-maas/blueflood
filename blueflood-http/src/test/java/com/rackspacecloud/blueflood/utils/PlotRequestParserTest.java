rax-maas/blueflood

commit 02c9fc30c54298d29d57313152fade6a921adecf

Handle the new rollup types properly on query
PlotRequestParser - Have a set of defaults for each rollup type, and a special marker set that indicates the default set should be used.
RollupHandler - Use generic `Points` instances and a utility method to compute `Points`->`Rollup`.
BasicRollupsOutputSerializer - add stats for various other fields, extract by rollup type (some extractions are invalid).
JSONBasicRollupsOutputSerializer - convert general default filter to type-specific default filter. add `toJSON()` conversion cases for the new types. make `getFilteredStatesForRollup` a moar generic.

blueflood-http/src/main/java/com/rackspacecloud/blueflood/outputs/utils/PlotRequestParser.java
@@ -19,18 +19,40 @@ package com.rackspacecloud.blueflood.outputs.utils;
 import com.rackspacecloud.blueflood.exceptions.InvalidRequestException;
 import com.rackspacecloud.blueflood.outputs.serializers.BasicRollupsOutputSerializer;
 import com.rackspacecloud.blueflood.types.Resolution;
-import com.rackspacecloud.blueflood.outputs.utils.RollupsQueryParams;
 
 import java.util.*;
 
 public class PlotRequestParser {
-    private static final Set<BasicRollupsOutputSerializer.MetricStat> defaultStats;
-
+    public static final Set<BasicRollupsOutputSerializer.MetricStat> DEFAULT_STATS = new HashSet<BasicRollupsOutputSerializer.MetricStat>();
+    public static final Set<BasicRollupsOutputSerializer.MetricStat> DEFAULT_BASIC;
+    public static final Set<BasicRollupsOutputSerializer.MetricStat> DEFAULT_COUNTER;
+    public static final Set<BasicRollupsOutputSerializer.MetricStat> DEFAULT_GAUGE;
+    public static final Set<BasicRollupsOutputSerializer.MetricStat> DEFAULT_SET;
+    public static final Set<BasicRollupsOutputSerializer.MetricStat> DEFAULT_TIMER;
+    
     static {
-        defaultStats = new HashSet<BasicRollupsOutputSerializer.MetricStat>();
-        defaultStats.add(BasicRollupsOutputSerializer.MetricStat.AVERAGE);
-        defaultStats.add(BasicRollupsOutputSerializer.MetricStat.NUM_POINTS);
-    }
+        // EnumSet is so crappy for making me do this instead of using an anonymous subclass.
+        DEFAULT_BASIC = EnumSet.noneOf(BasicRollupsOutputSerializer.MetricStat.class);
+        DEFAULT_COUNTER = EnumSet.noneOf(BasicRollupsOutputSerializer.MetricStat.class);
+        DEFAULT_GAUGE = EnumSet.noneOf(BasicRollupsOutputSerializer.MetricStat.class);
+        DEFAULT_SET = EnumSet.noneOf(BasicRollupsOutputSerializer.MetricStat.class);
+        DEFAULT_TIMER = EnumSet.noneOf(BasicRollupsOutputSerializer.MetricStat.class);
+        
+        DEFAULT_BASIC.add(BasicRollupsOutputSerializer.MetricStat.AVERAGE);
+        DEFAULT_BASIC.add(BasicRollupsOutputSerializer.MetricStat.NUM_POINTS);
+        
+        DEFAULT_COUNTER.add(BasicRollupsOutputSerializer.MetricStat.NUM_POINTS);
+        
+        DEFAULT_GAUGE.add(BasicRollupsOutputSerializer.MetricStat.NUM_POINTS);
+        DEFAULT_GAUGE.add(BasicRollupsOutputSerializer.MetricStat.LATEST);
+        
+        DEFAULT_SET.add(BasicRollupsOutputSerializer.MetricStat.NUM_POINTS);
+        
+        DEFAULT_TIMER.add(BasicRollupsOutputSerializer.MetricStat.RATE);
+        DEFAULT_TIMER.add(BasicRollupsOutputSerializer.MetricStat.NUM_POINTS);
+        DEFAULT_TIMER.add(BasicRollupsOutputSerializer.MetricStat.AVERAGE);
+        
+    } 
 
     public static RollupsQueryParams parseParams(Map<String, List<String>> params) throws InvalidRequestException {
         if (params == null || params.isEmpty()) {
@@ -75,7 +97,7 @@ public class PlotRequestParser {
 
     public static Set<BasicRollupsOutputSerializer.MetricStat> getStatsToFilter(List<String> select) {
         if (select == null || select.isEmpty()) {
-            return defaultStats;
+            return DEFAULT_STATS;
         } else {
             Set<BasicRollupsOutputSerializer.MetricStat> filters = new HashSet<BasicRollupsOutputSerializer.MetricStat>();
             // handle case when someone does select=average,min instead of select=average&select=min
@@ -84,7 +106,9 @@ public class PlotRequestParser {
                     List<String> nestedStats = Arrays.asList(stat.split(","));
                     filters.addAll(BasicRollupsOutputSerializer.MetricStat.fromStringList(nestedStats));
                 } else {
-                    filters.add(BasicRollupsOutputSerializer.MetricStat.fromString(stat));
+                    BasicRollupsOutputSerializer.MetricStat possibleStat = BasicRollupsOutputSerializer.MetricStat.fromString(stat);
+                    if (possibleStat != null)
+                        filters.add(possibleStat);
                 }
             }
             return filters;

blueflood-core/src/main/java/com/rackspacecloud/blueflood/outputs/handlers/RollupHandler.java
@@ -74,10 +74,11 @@ public class RollupHandler {
                 for (Range r : Range.rangesForInterval(g, latest + g.milliseconds(), to)) {
                     try {
                         MetricData data = AstyanaxReader.getInstance().getDatapointsForRange(locator, r, Granularity.FULL);
-                        Points<SimpleNumber> dataToRoll = data.getData();
-                        BasicRollup rollup = Rollup.BasicFromRaw.compute(dataToRoll);
-                        if (rollup.getCount() > 0) {
-                            metricData.getData().add(new Points.Point<BasicRollup>(r.getStart(), rollup));
+                        Points dataToRoll = data.getData();
+                        Rollup rollup = RollupHandler.rollupFromPoints(dataToRoll);
+                        
+                        if (rollup.hasData()) {
+                            metricData.getData().add(new Points.Point(minTime(dataToRoll), rollup));
                         }
 
                     } catch (IOException ex) {
@@ -96,6 +97,31 @@ public class RollupHandler {
         }
 
         return metricData;
+    }
+    
+    private static long minTime(Points<?> points) {
+        long min = Long.MAX_VALUE;
+        for (long time : points.getPoints().keySet())
+            min = Math.min(min, time);
+        return min;
+    }
+    
+    // note: similar thing happening in RollupRunnable.getRollupComputer(), but we don't have access to RollupType here.
+    private static Rollup rollupFromPoints(Points points) throws IOException {
+        Class rollupTypeClass = points.getDataClass();
+        if (rollupTypeClass.equals(SimpleNumber.class)) {
+            return Rollup.BasicFromRaw.compute(points);
+        } else if (rollupTypeClass.equals(CounterRollup.class)) {
+            return Rollup.CounterFromCounter.compute(points);
+        } else if (rollupTypeClass.equals(SetRollup.class)) {
+            return Rollup.SetFromSet.compute(points);
+        } else if (rollupTypeClass.equals(TimerRollup.class)) {
+            return Rollup.TimerFromTimer.compute(points);
+        } else if (rollupTypeClass.equals(GaugeRollup.class)) {
+            return Rollup.GaugeFromGauge.compute(points);
+        } else {
+            throw new IOException(String.format("Unexpected rollup type: %s", rollupTypeClass.getSimpleName()));
+        }
     }
 
     protected MetricData getHistogramsByGranularity(String tenantId,

blueflood-http/src/main/java/com/rackspacecloud/blueflood/outputs/serializers/BasicRollupsOutputSerializer.java
@@ -19,7 +19,11 @@ package com.rackspacecloud.blueflood.outputs.serializers;
 import com.rackspacecloud.blueflood.exceptions.SerializationException;
 import com.rackspacecloud.blueflood.outputs.formats.MetricData;
 import com.rackspacecloud.blueflood.types.BasicRollup;
+import com.rackspacecloud.blueflood.types.CounterRollup;
+import com.rackspacecloud.blueflood.types.GaugeRollup;
 import com.rackspacecloud.blueflood.types.Rollup;
+import com.rackspacecloud.blueflood.types.SetRollup;
+import com.rackspacecloud.blueflood.types.TimerRollup;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -33,8 +37,14 @@ public interface BasicRollupsOutputSerializer<T> {
     public static enum MetricStat {
         AVERAGE("average") {
             @Override
-            Object convertBasicRollupToObject(BasicRollup rollup) {
-                return rollup.getAverage();
+            Object convertRollupToObject(Rollup rollup) throws Exception {
+                if (rollup instanceof BasicRollup)
+                    return ((BasicRollup) rollup).getAverage();
+                else if (rollup instanceof TimerRollup)
+                    return ((TimerRollup) rollup).getAverage();
+                else
+                    // counters, sets
+                    throw new Exception(String.format("average not supported for this type: %s", rollup.getClass().getSimpleName()));
             }
 
             @Override
@@ -44,8 +54,14 @@ public interface BasicRollupsOutputSerializer<T> {
         },
         VARIANCE("variance") {
             @Override
-            Object convertBasicRollupToObject(BasicRollup rollup) {
-                return rollup.getVariance();
+            Object convertRollupToObject(Rollup rollup) throws Exception {
+                if (rollup instanceof BasicRollup)
+                    return ((BasicRollup) rollup).getVariance();
+                else if (rollup instanceof TimerRollup)
+                    return ((TimerRollup) rollup).getVariance();
+                else
+                    // counters, sets.
+                    throw new Exception(String.format("variance not supported for this type: %s", rollup.getClass().getSimpleName()));
             }
 
             @Override
@@ -55,8 +71,14 @@ public interface BasicRollupsOutputSerializer<T> {
         },
         MIN("min") {
             @Override
-            Object convertBasicRollupToObject(BasicRollup rollup) {
-                return rollup.getMinValue();
+            Object convertRollupToObject(Rollup rollup) throws Exception {
+                if (rollup instanceof BasicRollup)
+                    return ((BasicRollup) rollup).getMinValue();
+                else if (rollup instanceof TimerRollup)
+                    return ((TimerRollup) rollup).getMinValue();
+                else
+                    // counters, sets.
+                    throw new Exception(String.format("min not supported for this type: %s", rollup.getClass().getSimpleName()));
             }
 
             @Override
@@ -66,8 +88,14 @@ public interface BasicRollupsOutputSerializer<T> {
         },
         MAX("max") {
             @Override
-            Object convertBasicRollupToObject(BasicRollup rollup) {
-                return rollup.getMaxValue();
+            Object convertRollupToObject(Rollup rollup) throws Exception {
+                if (rollup instanceof BasicRollup)
+                    return ((BasicRollup) rollup).getMaxValue();
+                else if (rollup instanceof TimerRollup)
+                    return ((TimerRollup) rollup).getMaxValue();
+                else
+                    // counters, sets.
+                    throw new Exception(String.format("min not supported for this type: %s", rollup.getClass().getSimpleName()));
             }
 
             @Override
@@ -77,15 +105,89 @@ public interface BasicRollupsOutputSerializer<T> {
         },
         NUM_POINTS("numPoints") {
             @Override
-            Object convertBasicRollupToObject(BasicRollup rollup) {
-                return rollup.getCount();
+            Object convertRollupToObject(Rollup rollup) throws Exception {
+                if (rollup instanceof BasicRollup)
+                    return ((BasicRollup) rollup).getCount();
+                else if (rollup instanceof TimerRollup)
+                    return ((TimerRollup) rollup).getCount();
+                else if (rollup instanceof CounterRollup)
+                    return ((CounterRollup) rollup).getCount();
+                else if (rollup instanceof SetRollup)
+                    return ((SetRollup) rollup).getCount();
+                else
+                    // gauge.
+                    throw new Exception(String.format("numPoints not supported for this type: %s", rollup.getClass().getSimpleName()));
             }
 
             @Override
             Object convertRawSampleToObject(Object rawSample) {
                 return 1;
             }
-        };
+        },
+        LATEST("latest") {
+            @Override
+            Object convertRollupToObject(Rollup rollup) throws Exception {
+                if (rollup instanceof GaugeRollup)
+                    return ((GaugeRollup) rollup).getLatestValue().getValue();
+                else
+                    // every other type.
+                    throw new Exception(String.format("latest value not supported for this type: %s", rollup.getClass().getSimpleName()));
+            }
+
+            @Override
+            Object convertRawSampleToObject(Object rawSample) {
+                return rawSample;
+            }
+        },
+        RATE("rate") {
+            @Override
+            Object convertRollupToObject(Rollup rollup) throws Exception {
+                if (rollup instanceof TimerRollup)
+                    return ((TimerRollup) rollup).getRate();
+                else if (rollup instanceof CounterRollup)
+                    return ((CounterRollup) rollup).getRate();
+                else
+                    // gauge, set, basic
+                    throw new Exception(String.format("rate not supported for this type: %s", rollup.getClass().getSimpleName()));
+            }
+
+            @Override
+            Object convertRawSampleToObject(Object rawSample) {
+                return rawSample;
+            }
+        },
+        SUM("sum") {
+            @Override
+            Object convertRollupToObject(Rollup rollup) throws Exception {
+                if (rollup instanceof TimerRollup)
+                    return ((TimerRollup) rollup).getSum();
+                else
+                    // every other type.
+                    throw new Exception(String.format("sum not supported for this type: %s", rollup.getClass().getSimpleName()));
+            }
+
+            @Override
+            Object convertRawSampleToObject(Object rawSample) {
+                return rawSample;
+            }
+        },
+        PERCENTILE("percentiles") {
+            @Override
+            Object convertRollupToObject(Rollup rollup) throws Exception {
+                if (rollup instanceof TimerRollup)
+                    return ((TimerRollup) rollup).getPercentiles();
+                else
+                    // every other type.
+                    throw new Exception(String.format("percentiles supported for this type: %s", rollup.getClass().getSimpleName()));
+            }
+
+            @Override
+            Object convertRawSampleToObject(Object rawSample) {
+                return rawSample;
+            }
+        }
+        ;
+        
         private MetricStat(String s) {
             this.stringRep = s;
         }
@@ -113,7 +215,7 @@ public interface BasicRollupsOutputSerializer<T> {
         public String toString() {
             return this.stringRep;
         }
-        abstract Object convertBasicRollupToObject(BasicRollup rollup);
+        abstract Object convertRollupToObject(Rollup rollup) throws Exception;
         abstract Object convertRawSampleToObject(Object rawSample);
     }
 }

blueflood-http/src/main/java/com/rackspacecloud/blueflood/outputs/serializers/JSONBasicRollupsOutputSerializer.java
@@ -18,22 +18,47 @@ package com.rackspacecloud.blueflood.outputs.serializers;
 
 import com.rackspacecloud.blueflood.exceptions.SerializationException;
 import com.rackspacecloud.blueflood.outputs.formats.MetricData;
+import com.rackspacecloud.blueflood.outputs.utils.PlotRequestParser;
 import com.rackspacecloud.blueflood.types.BasicRollup;
+import com.rackspacecloud.blueflood.types.CounterRollup;
+import com.rackspacecloud.blueflood.types.GaugeRollup;
 import com.rackspacecloud.blueflood.types.Points;
+import com.rackspacecloud.blueflood.types.Rollup;
+import com.rackspacecloud.blueflood.types.SetRollup;
 import com.rackspacecloud.blueflood.types.SimpleNumber;
+import com.rackspacecloud.blueflood.types.TimerRollup;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.Set;
 
 public class JSONBasicRollupsOutputSerializer implements BasicRollupsOutputSerializer<JSONObject> {
-
+    private static final Logger log = LoggerFactory.getLogger(JSONBasicRollupsOutputSerializer.class);
+    
     @Override
     public JSONObject transformRollupData(MetricData metricData, Set<MetricStat> filterStats)
             throws SerializationException {
         final JSONObject globalJSON = new JSONObject();
         final JSONObject metaObject = new JSONObject();
+        
+        // if no stats were entered, figure out what type we are dealing with and select out default stats. 
+        if (filterStats == PlotRequestParser.DEFAULT_STATS) {
+            Class dataClass = metricData.getData().getDataClass();
+            if (dataClass.equals(BasicRollup.class))
+                filterStats = PlotRequestParser.DEFAULT_BASIC;
+            else if (dataClass.equals(GaugeRollup.class))
+                filterStats = PlotRequestParser.DEFAULT_GAUGE;
+            else if (dataClass.equals(CounterRollup.class))
+                filterStats = PlotRequestParser.DEFAULT_COUNTER;
+            else if (dataClass.equals(SetRollup.class))
+                filterStats = PlotRequestParser.DEFAULT_SET;
+            else if (dataClass.equals(TimerRollup.class))
+                filterStats = PlotRequestParser.DEFAULT_TIMER;
+            // else, I got nothing.
+        }
 
         final JSONArray valuesArray = transformDataToJSONArray(metricData, filterStats);
 
@@ -66,7 +91,16 @@ public class JSONBasicRollupsOutputSerializer implements BasicRollupsOutputSeria
 
         JSONObject filterStatsObject = null;
         long numPoints = 1;
-        if (point.getData() instanceof BasicRollup) {
+        
+        
+        
+        // todo: adding getCount() to Rollup interface will simplify this block.
+        // because of inheritance, GaugeRollup needs to come before BasicRollup. sorry.
+        if (point.getData() instanceof GaugeRollup) {
+            GaugeRollup rollup = (GaugeRollup)point.getData();
+            numPoints += rollup.getCount();
+            filterStatsObject = getFilteredStatsForRollup(rollup, filterStats);
+        } else if (point.getData() instanceof BasicRollup) {
             numPoints = ((BasicRollup) point.getData()).getCount();
             filterStatsObject = getFilteredStatsForRollup((BasicRollup) point.getData(), filterStats);
         } else if (point.getData() instanceof SimpleNumber) {
@@ -78,6 +112,18 @@ public class JSONBasicRollupsOutputSerializer implements BasicRollupsOutputSeria
         } else if (point.getData() instanceof Boolean) {
             numPoints = 1;
             filterStatsObject = getFilteredStatsForBoolean((Boolean) point.getData());
+        } else if (point.getData() instanceof SetRollup) {
+            SetRollup rollup = (SetRollup)point.getData();
+            numPoints += rollup.getCount();
+            filterStatsObject = getFilteredStatsForRollup(rollup, filterStats);
+        } else if (point.getData() instanceof TimerRollup) {
+            TimerRollup rollup = (TimerRollup)point.getData();
+            numPoints += rollup.getCount();
+            filterStatsObject = getFilteredStatsForRollup(rollup, filterStats);
+        } else if (point.getData() instanceof CounterRollup) {
+            CounterRollup rollup = (CounterRollup)point.getData();
+            numPoints += rollup.getCount().longValue();
+            filterStatsObject = getFilteredStatsForRollup(rollup, filterStats);
         } else {
             throw new SerializationException("Unsupported data type for Point");
         }
@@ -102,14 +148,22 @@ public class JSONBasicRollupsOutputSerializer implements BasicRollupsOutputSeria
         return object;
     }
 
-    private JSONObject getFilteredStatsForRollup(BasicRollup rollup, Set<MetricStat> filterStats) {
+    private JSONObject getFilteredStatsForRollup(Rollup rollup, Set<MetricStat> filterStats) {
         final JSONObject filteredObject = new JSONObject();
         for (MetricStat stat : filterStats) {
-            filteredObject.put(stat.toString(), stat.convertBasicRollupToObject(rollup));
+            try {
+                Object filteredValue = stat.convertRollupToObject(rollup);
+                if (filteredValue instanceof Map)
+                    filteredObject.putAll((Map)filteredValue);
+                else
+                    filteredObject.put(stat.toString(), filteredValue);
+            } catch (Exception ex) {
+                log.warn(ex.getMessage(), ex);
+            }
         }
         return filteredObject;
     }
-
+    
     private JSONObject getFilteredStatsForFullRes(Object rawSample, Set<MetricStat> filterStats) {
         final JSONObject filteredObject = new JSONObject();
         if (rawSample instanceof String || rawSample instanceof Boolean) {

commit fd01529c40e169211b365f1c09daf19f1153bd9b

Query without select was not returning default stats.
This regression was introduced in 02c9fc3 (mea culpa).

blueflood-http/src/main/java/com/rackspacecloud/blueflood/outputs/utils/PlotRequestParser.java
@@ -52,6 +52,8 @@ public class PlotRequestParser {
         DEFAULT_TIMER.add(BasicRollupsOutputSerializer.MetricStat.NUM_POINTS);
         DEFAULT_TIMER.add(BasicRollupsOutputSerializer.MetricStat.AVERAGE);
         
+        DEFAULT_STATS.add(BasicRollupsOutputSerializer.MetricStat.AVERAGE);
+        DEFAULT_STATS.add(BasicRollupsOutputSerializer.MetricStat.NUM_POINTS);
     } 
 
     public static RollupsQueryParams parseParams(Map<String, List<String>> params) throws InvalidRequestException {

blueflood-http/src/test/java/com/rackspacecloud/blueflood/utils/PlotRequestParserTest.java
@@ -45,4 +45,9 @@ public class PlotRequestParserTest {
         Assert.assertTrue(filters.contains(BasicRollupsOutputSerializer.MetricStat.MIN));
         Assert.assertTrue(filters.contains(BasicRollupsOutputSerializer.MetricStat.MAX));
     }
+    
+    @Test
+    public void testDefaultStatsAreNotEmpty() {
+        Assert.assertTrue(PlotRequestParser.DEFAULT_BASIC.size() > 0);
+    }
 }