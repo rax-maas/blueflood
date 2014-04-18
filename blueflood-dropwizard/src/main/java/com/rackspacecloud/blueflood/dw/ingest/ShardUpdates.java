613ecad2286c3f8f12818e02a14169fa46453879
gdusbabek <gdusbabek@gmail.com>
2014-04-18 07:57:21 -0500

    Remove the latest-time-wins strategy employed by `ShardUpdates`. Also document its concurrency properties.

--- blueflood-dropwizard/src/main/java/com/rackspacecloud/blueflood/dw/ingest/ShardUpdates.java
+++ blueflood-dropwizard/src/main/java/com/rackspacecloud/blueflood/dw/ingest/ShardUpdates.java
@@ -5,18 +5,17 @@ import com.rackspacecloud.blueflood.service.ScheduleContext;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Reduce contention on the locks in a ScheduleContext object by keeping all of its updates until they can all be
+ * flushed at once.
+ * 
+ * This class is NOT threadsafe.
+ */
 public class ShardUpdates {
     private Map<Integer, Long> updates = new HashMap<Integer, Long>();
     
     public void update(long when, int shard) {
-        Long current = updates.get(shard);
-        if (current == null) {
-            updates.put(shard, when);
-        } else {
-            if (when > current) {
-                updates.put(shard, when);
-            }
-        }
+        updates.put(shard, when);
     }
     
     public void flush(ScheduleContext context) {
