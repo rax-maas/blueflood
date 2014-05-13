bceef6374463f60f998a9d82e7301107526d07c5
Lakshmi Kannan <lakshmi.kannan@rackspace.com>
2014-05-13 10:59:51 -0700

    Add instrumentation around scanMetrics and checkMeta

--- blueflood-core/src/main/java/com/rackspacecloud/blueflood/service/IncomingMetricMetadataAnalyzer.java
+++ blueflood-core/src/main/java/com/rackspacecloud/blueflood/service/IncomingMetricMetadataAnalyzer.java
@@ -16,6 +16,7 @@
 
 package com.rackspacecloud.blueflood.service;
 
+import com.codahale.metrics.Timer;
 import com.rackspacecloud.blueflood.exceptions.CacheException;
 import com.rackspacecloud.blueflood.cache.MetadataCache;
 import com.rackspacecloud.blueflood.exceptions.IncomingMetricException;
@@ -25,6 +26,7 @@ import com.rackspacecloud.blueflood.types.IMetric;
 import com.rackspacecloud.blueflood.types.Metric;
 import com.rackspacecloud.blueflood.types.MetricMetadata;
 import com.rackspacecloud.blueflood.types.Locator;
+import com.rackspacecloud.blueflood.utils.Metrics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,7 +36,9 @@ import java.util.List;
 
 public class IncomingMetricMetadataAnalyzer {
     private static final Logger log = LoggerFactory.getLogger(IncomingMetricMetadataAnalyzer.class);
-    
+    private static Timer scanMetricsTimer = Metrics.timer(IncomingMetricMetadataAnalyzer.class, "Scan meta for metrics");
+    private static Timer checkMetaTimer = Metrics.timer(IncomingMetricMetadataAnalyzer.class, "Check meta");
+
     private final MetadataCache cache;
     
     public IncomingMetricMetadataAnalyzer(MetadataCache cache) {
@@ -44,6 +48,7 @@ public class IncomingMetricMetadataAnalyzer {
     public Collection<IncomingMetricException> scanMetrics(Collection<IMetric> metrics) {
         List<IncomingMetricException> problems = new ArrayList<IncomingMetricException>();
 
+        Timer.Context ctx = scanMetricsTimer.time();
         for (IMetric metric : metrics) {
             try {
                 if (metric instanceof Metric) {
@@ -56,26 +61,33 @@ public class IncomingMetricMetadataAnalyzer {
                 log.warn(ex.getMessage(), ex);
             }
         }
+        ctx.stop();
+
         return problems;
     }
 
     private IncomingMetricException checkMeta(Locator locator, String key, String incoming) throws CacheException {
-        if (Configuration.getInstance().getBooleanProperty(CoreConfig.DISABLE_TYPE_UNIT_PROCESSING_CACHE_READ)) {
-            cache.put(locator, key, incoming);
-            return null;
-        } else {
-            String existing = cache.get(locator, key, String.class);
-            // always update the cache. it is smart enough to avoid needless writes.
-            cache.put(locator, key, incoming);
-            boolean differs = existing != null && !incoming.equals(existing);
-            if (differs) {
-                if (key.equals(MetricMetadata.UNIT.name().toLowerCase())) {
-                    return new IncomingUnitException(locator, existing, incoming);
-                } else {
-                    return new IncomingTypeException(locator, existing, incoming);
+        Timer.Context ctx = checkMetaTimer.time();
+        try {
+            if (Configuration.getInstance().getBooleanProperty(CoreConfig.DISABLE_TYPE_UNIT_PROCESSING_CACHE_READ)) {
+                cache.put(locator, key, incoming);
+                return null;
+            } else {
+                String existing = cache.get(locator, key, String.class);
+                // always update the cache. it is smart enough to avoid needless writes.
+                cache.put(locator, key, incoming);
+                boolean differs = existing != null && !incoming.equals(existing);
+                if (differs) {
+                    if (key.equals(MetricMetadata.UNIT.name().toLowerCase())) {
+                        return new IncomingUnitException(locator, existing, incoming);
+                    } else {
+                        return new IncomingTypeException(locator, existing, incoming);
+                    }
                 }
+                return null;
             }
-            return null;
+        } finally {
+            ctx.stop();
         }
     }
 
