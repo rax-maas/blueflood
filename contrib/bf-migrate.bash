3fff20730ccfcc3a6c71e48d52a2944fb9c3eaf4
gdusbabek <gdusbabek@gmail.com>
2014-06-04 14:46:20 -0500

    Remove hardcoded directories from bf-migrate script.

--- contrib/bf-migrate.bash
+++ contrib/bf-migrate.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-CP=/Users/gdusbabek/codes/github/blueflood/blueflood-all/target/blueflood-all-2.0.0-SNAPSHOT-jar-with-dependencies.jar:/Users/gdusbabek/codes/github/blueflood/blueflood-core/target/classes
+WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+CP=${WORKING_DIR}/../blueflood-all/target/blueflood-all-2.0.0-SNAPSHOT-jar-with-dependencies.jar:${WORKING_DIR}/../blueflood-core/target/classes
 
 java -classpath $CP com.rackspacecloud.blueflood.tools.ops.Migration $@
