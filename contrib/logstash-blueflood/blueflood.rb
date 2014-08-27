87b306c738dabc2e455d045d578bf820f39690fa
Tilottama Gaat <tilottama.gaat@rackspace.com>
2014-08-27 14:33:22 -0400

    adding logstash plugin for blueflood

---
+++ contrib/logstash-blueflood/blueflood.rb
@@ -0,0 +1,55 @@
+# encoding: utf-8
+require "logstash/outputs/base"
+require "logstash/namespace"
+
+class LogStash::Outputs::Http < LogStash::Outputs::Base
+  # This output lets you `POST` events to a
+  # Blueflood endpoint
+  #
+
+  config_name "blueflood"
+  milestone 1
+
+  # URL to use
+  config :url, :validate => :string, :required => :true
+
+  config :content_type, :validate => :string, :default => "application/json"
+  
+  config :port, :validate => :string	
+  config :tenant_id, :validate => :string	
+  config :metrics, :validate => :string
+
+  config :format, :validate => ["json"], :default => "json"
+
+  public
+  def register
+    require "ftw"
+    require "uri"
+    require "json"
+
+    @agent = FTW::Agent.new
+    @url = url+":"+port+"/v2.0/"+tenant_id+"/ingest"
+    @content_type = "application/json"
+  end # def register
+
+  public
+  def receive(event)
+    return unless output?(event)
+
+    request = @agent.post(event.sprintf(@url))
+    request["Content-Type"] = @content_type
+
+    begin
+    	puts @metrics
+	request.body = event.sprintf(@metrics)
+    	response = @agent.execute(request)
+
+      # Consume body to let this connection be reused
+    	rbody = ""
+    	response.read_body { |c| rbody << c }
+    	puts rbody
+    rescue Exception => e
+        @logger.error("Unhandled exception", :request => request.body, :response => response)#, :exception => e, :stacktrace => e.backtrace)
+    end
+  end # def receive
+end
