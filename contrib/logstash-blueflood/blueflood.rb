fc742c686de0f38b6fec7f2c7ce278334119a219
Tilottama Gaat <tilottama.gaat@rackspace.com>
2014-08-28 16:02:42 -0400

    Documented each field

--- contrib/logstash-blueflood/blueflood.rb
+++ contrib/logstash-blueflood/blueflood.rb
@@ -3,20 +3,52 @@ require "logstash/outputs/base"
 require "logstash/namespace"
 
 class LogStash::Outputs::Blueflood < LogStash::Outputs::Base
-  # This output lets you `POST` events to a
-  # Blueflood endpoint
-
+  # This output lets you pull metrics from your logs and emit them
+  # to your Blueflood endpoint. 
+  #
+  # Blueflood (http://blueflood.io) is a distributed, multi-tenant
+  # metrics processing solution, created by engineers at Rackspace
+  # Blueflood is capable of ingesting, rolling up and serving metrics 
+  # at a massive scale. 
+  #
+  # The metrics can either be sent as well-formed JSON 
+  # (examples: https://github.com/rackerlabs/blueflood/wiki/10minuteguide)
+  # or as a hash. 
+  # 
+  # Sample use case: You are logging the load average of your system every 30 seconds
+  # Using a grok filter and the Blueflood output plugin, you can send these metrics to
+  # Blueflood. See many such examples in our sample logstash conf files here
+  # https://github.com/rackerlabs/blueflood/tree/master/contrib/logstash-blueflood
+  
   config_name "blueflood"
   milestone 1
 
-  # URL to use
+  # This setting is the url of your Blueflood instance
+  # Sample value: http://127.0.0.1
   config :url, :validate => :string, :required => :true
+ 
+  # This setting is the port at which Blueflood listens for ingest requests.
+  # Sample value: 19000
   config :port, :validate => :string	
+  
+  # This setting is the id of the tenant for which you are sending metrics
+  # Blueflood is a multi-tenant metrics store. 
+  # Sample Value: My company  name ie tgCompany
   config :tenant_id, :validate => :string	
-  config :json_metrics, :validate => :string
-  config :hash_metrics, :validate => :hash, :default => {}
-  config :format, :validate => ["json","hash"], :default => "json"
 
+  # This setting is used to send well formed json that Blueflood expects
+  # Sample Value: '[{"collectionTime": 1376509892612, "ttlInSeconds": 172800, "metricValue": 66, "metricName":"example.metric.one"}]'
+  # See usage in conf file https://github.com/rackerlabs/blueflood/tree/master/contrib/logstash-blueflood/blueflood.conf
+  config :json_metrics, :validate => :string
+  
+  # This setting is used to send metrics as a hash of key value pairs
+  # Sample Value:  [ "hosts.%{@source_host}.load_avg.1m", "%{load_avg_1m}"]
+  # See usage in conf file https://github.com/rackerlabs/blueflood/tree/master/contrib/logstash-blueflood/blueflood-hash-metrics.conf
+  config :hash_metrics, :validate => :hash, :default => {}
+
+  # This setting is used to specify whether the settings are json or hash
+  config :format, :validate => ["json","hash"], :default => "json"
+  
   public
   def register
     require "ftw"
