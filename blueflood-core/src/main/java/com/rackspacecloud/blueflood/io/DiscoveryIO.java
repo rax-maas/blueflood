331d289dacc8279dfc9eb7eaeb8923c1b7e636e3
Tilottama Gaat <tilottama.gaat@rackspace.com>
2014-10-07 15:56:45 -0400

    Changing the interface of discovery io to be generic, use IMetric instead of Metric wherever possible

--- blueflood-core/src/main/java/com/rackspacecloud/blueflood/io/DiscoveryIO.java
+++ blueflood-core/src/main/java/com/rackspacecloud/blueflood/io/DiscoveryIO.java
@@ -2,9 +2,7 @@ package com.rackspacecloud.blueflood.io;
 
 import java.util.List;
 
-import com.rackspacecloud.blueflood.types.Metric;
-
 public interface DiscoveryIO {
-    public void insertDiscovery(List<Metric> metrics) throws Exception;
+    public void insertDiscovery(List<Object> metrics) throws Exception;
     public List<SearchResult> search(String tenant, String query) throws Exception;
 }
