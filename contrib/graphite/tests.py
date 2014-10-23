e0dbdda63936521676f7a7c8cc8ae15b31c28aeb
George Jahad <github@blackbirdsystems.net>
2014-10-22 17:47:54 -0700

    fixed mock to not depend on string interning

--- contrib/graphite/tests.py
+++ contrib/graphite/tests.py
@@ -5,6 +5,25 @@ import datetime
 
 from blueflood import TenantBluefloodFinder, auth
 
+#To run this test you need to set up the ssh tunnel and environment var as
+# described below
+
+# This needs to be an env var corresponding to bf0testenv1's api key:
+api_key = os.environ['RAX_API_KEY']
+
+#port 2500 needs a tunnel like so:
+#ssh -L 2500:localhost:2500 app00.iad.stage.bf.k1k.me
+no_auth_config = {'blueflood':
+                  { 'urls': ['http://127.0.0.1:2500'],
+                    'tenant': "000000"}}
+
+rax_auth_config = {'blueflood':
+                   {'username': 'bf0testenv1',
+                    'apikey': api_key,
+                    'urls': ['http://iad.metrics.api.rackspacecloud.com'],
+                    'authentication_module': 'rax_auth',
+                    'tenant': "836986"}}
+
 try:
   from graphite.storage import FindQuery
   print 'using graphite.storage.FindQuery'
@@ -20,25 +39,7 @@ except:
         self.startTime = startTime
         self.endTime = endTime
 
-# This needs to be an env var corresponding to bf0testenv1's api key:
-api_key = os.environ['RAX_API_KEY']
-
-rax_auth_config = {'blueflood':
-                   {'username': 'bf0testenv1',
-                    'apikey': api_key,
-                    'urls': ['http://iad.metrics.api.rackspacecloud.com'],
-                    'authentication_module': 'rax_auth',
-                    'tenant': "836986"}}
-
-#port 2500 needs a tunnel like so:
-#ssh -L 2500:localhost:2500 app00.iad.stage.bf.k1k.me
-
-no_auth_config = {'blueflood':
-                  { 'urls': ['http://127.0.0.1:2500'],
-                    'tenant': "000000"}}
-
 class BluefloodTests(TestCase):
-
   def test_conf(self):
     pass
 
@@ -49,15 +50,26 @@ class BluefloodTests(TestCase):
   def setup_UTC_mock(self):
     #setup a mock that forces expiration
     self.origGetCurrentUTC = type(auth.auth).getCurrentUTC
+    self.origDoAuth = type(auth.auth).doAuth
     this = self
+    self.authCount = 0
     def mockGetCurrentUTC(self):
       return this.origGetCurrentUTC(self) + datetime.timedelta(days=1)
+    def mockDoAuth(self):
+      this.authCount += 1
+      this.origDoAuth(self)
     type(auth.auth).getCurrentUTC = mockGetCurrentUTC
+    type(auth.auth).doAuth = mockDoAuth
 
   def unset_UTC_mock(self):
     type(auth.auth).getCurrentUTC = self.origGetCurrentUTC
+    type(auth.auth).doAuth = self.origDoAuth
 
   def test_finder(self):
+    #test no auth
+    finder = TenantBluefloodFinder(no_auth_config)
+    self.run_find(finder)
+
     #test rax auth
     finder = TenantBluefloodFinder(rax_auth_config)
     self.run_find(finder)
@@ -67,17 +79,10 @@ class BluefloodTests(TestCase):
     self.run_find(finder)
 
     #test expired UTC
-    old_token = auth.auth.token
     self.setup_UTC_mock()
     self.run_find(finder)
     self.unset_UTC_mock()
-    new_token = auth.auth.token
-    # Should be a new instance of the same token,(we forced expiration)
-    self.assertTrue(old_token is not new_token and old_token == new_token)
-
-    #test no auth
-    finder = TenantBluefloodFinder(no_auth_config)
-    self.run_find(finder)
+    self.assertTrue(self.authCount == 1)
 
 if __name__ == '__main__':
   unittest.main()
