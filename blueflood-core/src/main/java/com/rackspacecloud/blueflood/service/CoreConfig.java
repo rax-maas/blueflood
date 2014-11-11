b3e6eda98b67db184c391235e1e0b2c520aeb214
Tilottama Gaat <tilottama.gaat@rackspace.com>
2014-11-11 15:11:11 -0500

    Dropping all string metrics in staging

--- blueflood-core/src/main/java/com/rackspacecloud/blueflood/service/CoreConfig.java
+++ blueflood-core/src/main/java/com/rackspacecloud/blueflood/service/CoreConfig.java
@@ -140,8 +140,8 @@ public enum CoreConfig implements ConfigDefaults {
     // debugging only.
     SKIP_ROLLUP_WRITES("false"),
     
-    STRING_METRICS_DROPPED("false"),
-    TENANTIDS_TO_KEEP("");
+    STRING_METRICS_DROPPED("true"),
+    TENANTIDS_TO_KEEP("BOGUSTENANT123");
 
     static {
         Configuration.getInstance().loadDefaults(CoreConfig.values());
