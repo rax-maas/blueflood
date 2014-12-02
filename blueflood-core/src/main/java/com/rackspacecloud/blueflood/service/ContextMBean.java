21483091073a63abe347dfdfcf1202c624ab6b2e
Chinmay Gupte <chinmay.gupte@rackspace.com>
2014-12-01 17:08:38 -0800

    Actually the ContextMBean interface

---
+++ blueflood-core/src/main/java/com/rackspacecloud/blueflood/service/ContextMBean.java
@@ -0,0 +1,10 @@
+package com.rackspacecloud.blueflood.service;
+
+import java.util.Collection;
+
+public interface ContextMBean {
+    /**
+     * Get the metrics state currently stored by ScheduleContext
+     */
+    public Collection<String> getMetricsState(int shard, String gran, int slot);
+}
