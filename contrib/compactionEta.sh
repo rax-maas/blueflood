d4669a1e5ac875195c58ab8744a4f0e802e8f796
stackedsax <alex@alexscammon.com>
2015-01-01 16:27:36 -0800

    Add a script to check for status on compactions
    We need a way to see how much longer compactions will take.  This script adds that support.  It merely prints out the number of hours remaining for any major compaction running on a node in our cluster.
    
    Run with:
    
      parallel -j20 "ssh db{1}.iad.prod.bf.k1k.me 'bash -s' < ./compactionEta.sh 9080" ::: 00
    01
    02
    03
    04
    05
    06
    07
    08
    09
    10
    11
    12
    13
    14
    15
    16
    17
    18
    19
    20
    21
    22
    23
    24
    25
    26
    27
    28
    29
    30
    31 | sort
    
    (oh, yeah, install GNU parallel (brew install parallel on a Mac) before doing it this way)

---
+++ contrib/compactionEta.sh
@@ -0,0 +1,52 @@
+#/bin/bash
+
+# Description: a script to see how much time is left on major 
+#              compactions on cassandra nodes
+#
+# Instructions:
+#
+# a. To run for a single machine run the command below.  
+#    You can change the port, or leave blank to use the default port for nodetool:
+#
+#       ssh db01.iad.prod.bf.k1k.me 'bash -s' < ./compactionEta.sh [port]
+#
+# b. To run for multiple machines, replace 'domain.com' with the 
+#    appropriate domain and <port> with the port that cassandra runs on.
+#    This assumes you number your hosts like my team does, but hopefully 
+#    you get the idea.
+#
+#       for x in {00..31}; do ssh db$(printf "%2.2d" $x).domain.com 'bash -s' < ./compactionEta.sh [port]; done
+#
+#
+# c. Or, to run on multiple machines a little faster, install GNU parallel 
+#    and run the command below. Again, replace 'domain.com' and also <start> 
+#    and <end> with the first and last servers you want to check.
+# 
+#       parallel -j20 --no-notice "ssh db{1}.domain.com 'bash -s' < ./compactionEta.sh [port]" ::: $(seq -w <start> <end>) | sort
+
+
+if [ $1 ]; then 
+	PORT=$1
+else
+	PORT=7199
+fi
+
+# First we'll grab the seconds elapsed since a major compaction was started
+SECONDS_ELAPSED=$(ps -p $(ps auxx | grep nodetool | grep compact | awk '{print $2}') -o etimes= 2>/dev/null)
+
+# If a major compaction is running, ask nodetool how far along it is
+# We will grab the largest one, assuming that it's the one we want
+if [ -z $SECONDS_ELAPSED ]; then 
+    echo $(hostname)": Node down or not running a major compaction"
+    exit 2
+else
+    PERCENT_COMPLETE=$(/opt/cassandra/bin/nodetool -p $PORT compactionstats 2>/dev/null | grep 'metrics_full' | sort -n -k 5 | tail -1 | awk '{print $7}' | tr -d %)
+fi
+
+# Assuming we got $PERCENT_COMPLETE just fine, print out how much time is remaining
+if [ $SECONDS_ELAPSED -a $PERCENT_COMPLETE ]; then 
+    echo "$SECONDS_ELAPSED $PERCENT_COMPLETE" | awk -v HOST=$(hostname) '{printf HOST ":%8.2f hours\n", (($1*100/$2)-$1)/3600}'
+else
+    echo $(hostname)": Node down or not running a major compaction"
+    exit 2
+fi 
