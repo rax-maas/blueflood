b978132846d6ea4ee4f5a7be73232007f6180e27
George Jahad <github@blackbirdsystems.net>
2015-01-09 20:30:01 -0800

    added preagg checks

--- blueflood-core/src/test/java/com/rackspacecloud/blueflood/inputs/processors/BatchWriterTest.java
+++ blueflood-core/src/test/java/com/rackspacecloud/blueflood/inputs/processors/BatchWriterTest.java
@@ -7,6 +7,7 @@ import com.rackspacecloud.blueflood.io.IMetricsWriter;
 import com.rackspacecloud.blueflood.service.IngestionContext;
 import com.rackspacecloud.blueflood.types.IMetric;
 import com.rackspacecloud.blueflood.types.Metric;
+import com.rackspacecloud.blueflood.types.PreaggregatedMetric;
 import com.rackspacecloud.blueflood.types.Locator;
 import com.rackspacecloud.blueflood.utils.TimeValue;
 import com.rackspacecloud.blueflood.utils.Util;
@@ -20,35 +21,36 @@ import org.junit.Test;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.stub;
+import static org.mockito.Mockito.times;
 
 
 public class BatchWriterTest {
     private static TimeValue timeout = new TimeValue(5, TimeUnit.SECONDS);
 
-    static List<List<IMetric>> createTestData() {
+    static <T extends IMetric> List<List<IMetric>> createTestData(Class<T> c) {
         List<IMetric> l1 = new ArrayList<IMetric>();
-        l1.add(mock(Metric.class));
-        l1.add(mock(Metric.class));
-        l1.add(mock(Metric.class));
-        l1.add(mock(Metric.class));
+        l1.add(mock(c));
+        l1.add(mock(c));
+        l1.add(mock(c));
+        l1.add(mock(c));
 
         List<IMetric> l2 = new ArrayList<IMetric>();
-        l2.add(mock(Metric.class));
-        l2.add(mock(Metric.class));
-        l2.add(mock(Metric.class));
-        l2.add(mock(Metric.class));
+        l2.add(mock(c));
+        l2.add(mock(c));
+        l2.add(mock(c));
+        l2.add(mock(c));
 
         List<IMetric> l3 = new ArrayList<IMetric>();
-        l3.add(mock(Metric.class));
-        l3.add(mock(Metric.class));
-        l3.add(mock(Metric.class));
-        l3.add(mock(Metric.class));
+        l3.add(mock(c));
+        l3.add(mock(c));
+        l3.add(mock(c));
+        l3.add(mock(c));
 
         List<IMetric> l4 = new ArrayList<IMetric>();
-        l4.add(mock(Metric.class));
-        l4.add(mock(Metric.class));
-        l4.add(mock(Metric.class));
-        l4.add(mock(Metric.class));
+        l4.add(mock(c));
+        l4.add(mock(c));
+        l4.add(mock(c));
+        l4.add(mock(c));
 
 
         // create fake metrics to test
@@ -80,27 +82,44 @@ public class BatchWriterTest {
         IMetricsWriter writer = mock(IMetricsWriter.class);
         Counter bufferedMetrics = mock(Counter.class);
         IngestionContext context = mock(IngestionContext.class);
-        List<List<IMetric>> testdata = createTestData();
+        List<List<IMetric>> testdata = createTestData(Metric.class);
+        List<List<IMetric>> pTestdata = createTestData(PreaggregatedMetric.class);
+        List<List<IMetric>> allTestdata = new ArrayList<List<IMetric>>();
+        allTestdata.addAll(testdata);
+        allTestdata.addAll(pTestdata);
+
         BatchWriter batchWriter = new BatchWriter(
                     new ThreadPoolBuilder().build(),
                     writer,
                     timeout,
                     bufferedMetrics,
                     context);
-
-        ListenableFuture<List<Boolean>> futures = batchWriter.apply(testdata);
+        
+        ListenableFuture<List<Boolean>> futures = batchWriter.apply(allTestdata);
         List<Boolean> persisteds = futures.get(timeout.getValue(), timeout.getUnit());
 
         //Confirm that each batch finished
         for (Boolean batchStatus : persisteds) {
             Assert.assertTrue(batchStatus);
         }
-        //Confirm that each batch was inserted
+
+        //Confirm that each regular batch was inserted
         for (List<IMetric> l : testdata) {
-          verify(writer).insertFullMetrics((List<Metric>)(List<?>)l);
-          for (IMetric m : l) {
-              verify(context).update(m.getCollectionTime(), Util.getShard(m.getLocator().toString()));
-          }
+            verify(writer).insertFullMetrics((List<Metric>)(List<?>)l);
+        }
+
+        //Confirm that each preagg batch was inserted
+        for (List<IMetric> l : pTestdata) {
+            verify(writer).insertPreaggreatedMetrics(l);
+        }
+
+        //Confirm scheduleContext was updated
+        for (List<IMetric> l : allTestdata) {
+            for (IMetric m : l) {
+                //Use times(2) below because each of 
+                verify(context, times(2)).update(m.getCollectionTime(), 
+                    Util.getShard(m.getLocator().toString()));
+            }
         }
     }
 }
