94090a27ab90092a9b2d61e9e93855a0bed7c5ae
Tilottama Gaat <tilottama.gaat@rackspace.com>
2015-02-04 12:53:43 -0800

    Adding tests

--- blueflood-core/src/test/java/com/rackspacecloud/blueflood/inputs/processors/RollupTypeCacherTest.java
+++ blueflood-core/src/test/java/com/rackspacecloud/blueflood/inputs/processors/RollupTypeCacherTest.java
@@ -17,48 +17,22 @@
 package com.rackspacecloud.blueflood.inputs.processors;
 import com.rackspacecloud.blueflood.cache.MetadataCache;
 import com.rackspacecloud.blueflood.concurrent.ThreadPoolBuilder;
-import com.rackspacecloud.blueflood.types.IMetric;
-import com.rackspacecloud.blueflood.types.Metric;
-import com.rackspacecloud.blueflood.types.Locator;
-import com.rackspacecloud.blueflood.types.MetricsCollection;
-import com.rackspacecloud.blueflood.types.MetricMetadata;
-import com.rackspacecloud.blueflood.types.RollupType;
+import com.rackspacecloud.blueflood.types.*;
 
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.stub;
-import static org.mockito.Mockito.verify;
+
+import static org.mockito.Mockito.*;
 
 public class RollupTypeCacherTest {
     private static final int METRICS_PER_LIST = 4;
     private static final String cacheKey = MetricMetadata.ROLLUP_TYPE.name().toLowerCase();
 
-    static MetricsCollection createTestData() {
-        // create fake metrics to test
-        Integer counter = 0;
-        List<IMetric> l = new ArrayList<IMetric>();
-        for (int j = 0; j < METRICS_PER_LIST; j++) {
-            counter++;
-            IMetric m = mock(Metric.class);
-            l.add(m);
-            // setup the metric stubs
-            stub(m.getLocator()).
-                toReturn(Locator.createLocatorFromDbKey(counter.toString()));
-            stub(m.getCollectionTime()).toReturn(counter.longValue());
-            stub(m.getRollupType()).toReturn(RollupType.BF_BASIC);
-        }
-        
-        MetricsCollection collection = new MetricsCollection();
-        collection.add(new ArrayList<IMetric>(l));
-        return collection;
-    }
-
     @Test
-    public void testCacher() throws Exception {
+    public void testCacherDoesNotCacheRollupTypeForMetrics() throws Exception {
         MetricsCollection collection = createTestData();
 
         MetadataCache rollupTypeCache = mock(MetadataCache.class);
@@ -75,10 +49,66 @@ public class RollupTypeCacherTest {
             Thread.sleep(1);
         }
 
+        verifyZeroInteractions(rollupTypeCache);
+    }
+
+    @Test
+    public void testCacherDoesCacheRollupTypeForPreAggregatedMetrics() throws Exception {
+        MetricsCollection collection = createPreaggregatedTestMetrics();
+
+        MetadataCache rollupTypeCache = mock(MetadataCache.class);
+        ThreadPoolExecutor tpe =
+                new ThreadPoolBuilder().withName("rtc test").build();
+
+        RollupTypeCacher rollupTypeCacher = new RollupTypeCacher(tpe,
+                rollupTypeCache);
+
+        rollupTypeCacher.apply(collection);
+
+        // wait till done
+        while (tpe.getCompletedTaskCount() < 1) {
+            Thread.sleep(1);
+        }
+
         // Confirm that each metric is cached
         for (IMetric m : collection.toMetrics()) {
-            verify(rollupTypeCache).put(m.getLocator(), cacheKey, 
-                m.getRollupType().toString());
+            verify(rollupTypeCache).put(m.getLocator(), cacheKey,
+                    m.getRollupType().toString());
         }
+    }
+
+    private static MetricsCollection createPreaggregatedTestMetrics() {
+        List<IMetric> listMetrics = new ArrayList<IMetric>();
+        for (int j=0; j < METRICS_PER_LIST; j++) {
+            IMetric m = mock(PreaggregatedMetric.class);
+            listMetrics.add(m);
+
+            stub(m.getLocator()).toReturn(Locator.createLocatorFromDbKey("Counter" + j));
+            stub(m.getCollectionTime()).toReturn(1234500000+(long)j);
+            stub(m.getRollupType()).toReturn(RollupType.COUNTER);
+        }
+        MetricsCollection collection = new MetricsCollection();
+        collection.add(listMetrics);
+        return collection;
+    }
+
+    private static MetricsCollection createTestData() {
+        // create fake metrics to test
+        Integer counter = 0;
+        List<IMetric> l = new ArrayList<IMetric>();
+        for (int j = 0; j < METRICS_PER_LIST; j++) {
+            counter++;
+            IMetric m = mock(Metric.class);
+            l.add(m);
+            // setup the metric stubs
+            stub(m.getLocator()).
+                    toReturn(Locator.createLocatorFromDbKey(counter.toString()));
+            stub(m.getCollectionTime()).toReturn(counter.longValue());
+            stub(m.getRollupType()).toReturn(RollupType.BF_BASIC);
+        }
+
+        MetricsCollection collection = new MetricsCollection();
+        collection.add(new ArrayList<IMetric>(l));
+        return collection;
     }
 }
