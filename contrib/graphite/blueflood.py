0f55a83721dc6e42410bcf24071cbf401a7bc614
George Jahad <github@blackbirdsystems.net>
2015-03-25 16:46:25 -0700

    fetch_multi seems to work

--- contrib/graphite/blueflood.py
+++ contrib/graphite/blueflood.py
@@ -57,29 +57,31 @@ def grouper(iterable, n, fillvalue=None):
   return itertools.izip_longest(fillvalue=fillvalue, *args)
 
 
-
 class TenantBluefloodFinder(object):
-  #__fetch_multi__ = 'tenant_blueflood'
+  __fetch_multi__ = 'tenant_blueflood'
   def __init__(self, config=None):
-    print("gbj v6")
+    print("gbj v7")
     if os.path.isfile("/root/pdb-flag"):
       remote_pdb.RemotePdb('127.0.0.1', 4444).set_trace()
-    authentication_module = None
+
     if config is not None:
-      if 'urls' in config['blueflood']:
-        urls = config['blueflood']['urls']
-      else:
-        urls = [config['blueflood']['url'].strip('/')]
-      tenant = config['blueflood']['tenant']
-      if 'authentication_module' in config['blueflood']:
-        authentication_module = config['blueflood']['authentication_module']
-        authentication_class = config['blueflood']['authentication_class']
+      bf_config = config.get('blueflood', {})
+      urls = bf_config.get('urls', bf_config.get('url', '').strip('/'))
+      tenant = bf_config.get('tenant', None)
+      authentication_module =  bf_config.get('authentication_module', None)
+      authentication_class =  bf_config.get('authentication_class', None)
+      default_submetric = bf_config.get('default_submetric', 'average')
+      enable_submetrics = bf_config.get('enable_submetrics', False)
+      submetric_aliases = bf_config.get('submetric_names', {})
     else:
       from django.conf import settings
       urls = getattr(settings, 'BF_QUERY')
       tenant = getattr(settings, 'BF_TENANT')
       authentication_module = getattr(settings, 'BF_AUTHENTICATION_MODULE', None)
       authentication_class = getattr(settings, 'BF_AUTHENTICATION_CLASS', None)
+      default_submetric = getattr(settings, 'BF_DEFAULT_SUBMETRIC', 'average')
+      enable_submetrics = getattr(settings, 'BF_ENABLE_SUBMETRICS', False)
+      submetric_aliases = getattr(settings, 'BF_SUBMETRIC_NAMES', {})
 
     if authentication_module:
       module = __import__(authentication_module)
@@ -89,31 +91,39 @@ class TenantBluefloodFinder(object):
 
     self.tenant = tenant
     self.bf_query_endpoint = urls[0]
-
+    self.enable_submetrics = enable_submetrics
+    self.submetric_names = submetric_names
 
   def find_nodes(self, query):
-    try:
-      query_depth = len(query.pattern.split('.'))
-      #print 'DAS QUERY ' + str(query_depth) + ' ' + query.pattern
-      client = Client(self.bf_query_endpoint, self.tenant)
-      values = client.find_metrics(query.pattern)
-      
-      for obj in values:
-        metric = obj['metric']
-        parts = metric.split('.')
-        metric_depth = len(parts)
-        if metric_depth > query_depth:
-          yield BranchNode('.'.join(parts[:query_depth]))
+    query_parts = query.pattern.split('.')
+
+    query_depth = len(query_parts)
+
+    client = BluefloodClient(self.bf_query_endpoint, self.tenant)
+
+    for metric in client.find_metrics(query.pattern):
+      metric_parts = metric.split('.')
+
+      if query_depth < len(metric_parts):
+        yield BranchNode('.'.join(metric_parts[:query_depth]))
+
+      elif len(metric_parts) == query_depth:
+        if self.enable_submetrics:
+          for alias, select in self.submetric_aliases.items():
+            graphite_metric = '.'.join(metric_parts + [select])
+            yield TenantBluefloodLeafNode(graphite_metric, TenantBluefloodReader(metric, select, self.tenant, self.bf_query_endpoint))
         else:
-          yield LeafNode(metric, TenantBluefloodReader(metric, self.tenant, self.bf_query_endpoint))
-    except Exception as e:
-     print "Exception in Blueflood find_nodes: " 
-     print e
-     exc_info = sys.exc_info()
-     tb = traceback.format_exception(*exc_info)
-     for line in tb:
-       print(line)
-     raise e
+          yield TenantBluefloodLeafNode(graphite_metric, TenantBluefloodReader(metric, self.default_submetric, self.tenant, self.bf_query_endpoint))
+    # try:
+    #   pass
+    # except Exception as e:
+    #  print "Exception in Blueflood find_nodes: "
+    #  print e
+    #  exc_info = sys.exc_info()
+    #  tb = traceback.format_exception(*exc_info)
+    #  for line in tb:
+    #    print(line)
+    #  raise e
 
   def get_metric_list(self, endpoint, tenant, metric_list, payload, headers):
     url = "%s/v2.0/%s/views" % (endpoint, tenant)
@@ -131,6 +141,7 @@ class TenantBluefloodFinder(object):
       return r.json()['metrics']
 
   def fetch_multi(self, nodes, start_time, end_time):
+    print("gbj fm on ")
     paths = [node.path for node in nodes]
     res = calc_res(start_time, end_time)
     step = secs_per_res[res]
@@ -144,21 +155,24 @@ class TenantBluefloodFinder(object):
     # Limit size MPlot requests
     groups = [filter(None,l) for l in list(grouper(paths, 40, False))]
     responses = reduce(lambda x,y: x+y,
-                         [self.get_metric_list(self.bf_query_endpoint, 
-                                               self.tenant, g, payload, headers) 
+                         [self.get_metric_list(self.bf_query_endpoint,
+                                               self.tenant, g, payload, headers)
                           for g in groups])
+    client = BluefloodClient(self.bf_query_endpoint, self.tenant)
     cache = {x['metric'] : client.process_path(x['data'], start_time, real_end_time, step)
              for x in responses}
     time_info = (start_time, real_end_time, step)
     return (time_info, cache)
 
+
 class TenantBluefloodReader(object):
   __slots__ = ('metric', 'tenant', 'bf_query_endpoint')
   supported = True
 
-  def __init__(self, metric, tenant, endpoint):
+  def __init__(self, metric, submetric, tenant, endpoint):
     # print 'READER ' + tenant + ' ' + metric
     self.metric = metric
+    self.submetric = submetric
     self.tenant = tenant
     self.bf_query_endpoint = endpoint
 
@@ -176,7 +190,7 @@ class TenantBluefloodReader(object):
     else:
       res = calc_res(start_time, end_time)
       step = secs_per_res[res]
-      client = Client(self.bf_query_endpoint, self.tenant)
+      client = BluefloodClient(self.bf_query_endpoint, self.tenant)
       values = client.get_values(self.metric, start_time, end_time, res)
       real_end_time = end_time + step
       processed_values = client.process_path(values, start_time, real_end_time, step)
@@ -186,7 +200,7 @@ class TenantBluefloodReader(object):
         print("gbj44 ", processed_values[0])
       return (time_info, processed_values)
 
-class Client(object):
+class BluefloodClient(object):
   def __init__(self, host, tenant):
     self.host = host
     self.tenant = tenant
@@ -205,17 +219,23 @@ class Client(object):
     payload = {'query': query}
     headers = auth.headers()
     r = self.make_request("%s/v2.0/%s/metrics/search" % (self.host, self.tenant), payload, headers)
-    if r.status_code != 200:
-      print str(r.status_code) + ' in find_metrics ' + r.url + ' ' + r.text
-      return []
+
+    if r.status_code == 200:
+        return [m['metric'] for m in r.json()]
     else:
-      try:
-        return r.json()
-      except TypeError:
-        # we need to parse the json ourselves.
-        return json.loads(r.text)
-      except ValueError:
-        return ['there was an error']
+      return []
+
+    # != 200:
+    #   print str(r.status_code) + ' in find_metrics ' + r.url + ' ' + r.text
+    #   return []
+    # else:
+    #   try:
+    #     return r.json()
+    #   except TypeError:
+    #     # we need to parse the json ourselves.
+    #     return json.loads(r.text)
+    #   except ValueError:
+    #     return ['there was an error']
 
   def get_values(self, metric, start, stop, res):
     payload = {
@@ -280,10 +300,11 @@ class Client(object):
         ret_arr.append(v_iter[0][data_key])
       else:
         ret_arr.append(None)
-      
+
     return ret_arr
 
-#class TenantBluefloodLeafNode(LeafNode):
-  #__fetch_multi__ = 'tenant_blueflood'
+
+class TenantBluefloodLeafNode(LeafNode):
+  __fetch_multi__ = 'tenant_blueflood'
 
 
