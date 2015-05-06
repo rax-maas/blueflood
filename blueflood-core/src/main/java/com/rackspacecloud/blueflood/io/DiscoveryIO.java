136eb35dbce34cd64859831b700c3585a83599fe
Chinmay Gupte <chinmay.gupte@rackspace.com>
2015-05-06 15:31:50 -0700

    Some minor nit fixes

--- blueflood-core/src/main/java/com/rackspacecloud/blueflood/io/DiscoveryIO.java
+++ blueflood-core/src/main/java/com/rackspacecloud/blueflood/io/DiscoveryIO.java
@@ -1,12 +1,11 @@
 package com.rackspacecloud.blueflood.io;
 
 import com.rackspacecloud.blueflood.types.IMetric;
-import com.rackspacecloud.blueflood.types.Locator;
 
 import java.util.List;
 
 public interface DiscoveryIO {
     public void insertDiscovery(List<IMetric> metrics) throws Exception;
     public List<SearchResult> search(String tenant, String query) throws Exception;
-    public List<SearchResult> search(String tenant, List<String> locators);
+    public List<SearchResult> search(String tenant, List<String> queries);
 }
