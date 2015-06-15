286795610db39743deb50925490fcd7d8d982859
Chinmay Gupte <chinmay.gupte@rackspace.com>
2015-06-15 13:50:55 -0700

    Add comments to the test

--- blueflood-elasticsearch/src/test/java/com/rackspacecloud/blueflood/service/ElasticIOTest.java
+++ blueflood-elasticsearch/src/test/java/com/rackspacecloud/blueflood/service/ElasticIOTest.java
@@ -242,18 +242,23 @@ public class ElasticIOTest {
 
     @Test
     public void testDeDupMetrics() throws Exception {
+        // Create another index
         String ES_DUP = ElasticIO.INDEX_NAME_WRITE + "_2";
         esSetup.execute(EsSetup.createIndex(ES_DUP)
                 .withMapping("metrics", EsSetup.fromClassPath("metrics_mapping_v1.json")));
+        // Insert metric into the new index
         elasticIO.setINDEX_NAME_WRITE(ES_DUP);
         ArrayList metricList = new ArrayList();
         metricList.add(new Metric(createTestLocator(TENANT_A, 0, "A", 0), "blarg", 0, new TimeValue(1, TimeUnit.DAYS), UNIT));
         elasticIO.insertDiscovery(metricList);
         esSetup.client().admin().indices().prepareRefresh().execute().actionGet();
+        // Set up aliases
         esSetup.client().admin().indices().prepareAliases().addAlias(ES_DUP, "metric_metadata_read")
                 .addAlias(ElasticIO.INDEX_NAME_WRITE, "metric_metadata_read").execute().actionGet();
         elasticIO.setINDEX_NAME_READ("metric_metadata_read");
         List<SearchResult> results = elasticIO.search(TENANT_A, "one.two.three00.fourA.five0");
+        // Should just be one result
         Assert.assertEquals(results.size(), 1);
+        Assert.assertEquals(results.get(0).getMetricName(), "one.two.three00.fourA.five0");
     }
 }
