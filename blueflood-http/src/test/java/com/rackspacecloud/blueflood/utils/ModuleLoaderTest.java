2029d32399e7cb015943da299e80d02a4af2c16e
Gaurav Bajaj <gauravbajaj@Gauravs-MacBook-Pro-2.local>
2015-06-15 15:28:28 -0700

    Additional test cases

--- blueflood-http/src/test/java/com/rackspacecloud/blueflood/utils/ModuleLoaderTest.java
+++ blueflood-http/src/test/java/com/rackspacecloud/blueflood/utils/ModuleLoaderTest.java
@@ -11,14 +11,17 @@ public class ModuleLoaderTest {
 
     @Test
     public void getInstanceShouldReturnCorrectInstance(){
-
         System.setProperty("EVENTS_MODULES", "");
         Assert.assertNull(ModuleLoader.getInstance(GenericElasticSearchIO.class, CoreConfig.EVENTS_MODULES));
 
         System.setProperty("DISCOVERY_MODULES", "com.rackspacecloud.blueflood.io.ElasticIO");
-        Assert.assertTrue((ModuleLoader.getInstance(DiscoveryIO.class, CoreConfig.DISCOVERY_MODULES)) instanceof com.rackspacecloud.blueflood.io.DiscoveryIO);
+        Assert.assertTrue((ModuleLoader.getInstance(DiscoveryIO.class, CoreConfig.DISCOVERY_MODULES)) instanceof DiscoveryIO);
 
+        System.setProperty("EVENTS_MODULES", "com.rackspacecloud.blueflood.io.EventElasticSearchIO");
+        Assert.assertTrue((ModuleLoader.getInstance(GenericElasticSearchIO.class, CoreConfig.EVENTS_MODULES)) instanceof GenericElasticSearchIO);
 
+        Assert.assertFalse((ModuleLoader.getInstance(GenericElasticSearchIO.class, CoreConfig.EVENTS_MODULES)) instanceof DiscoveryIO);
+        Assert.assertFalse((ModuleLoader.getInstance(DiscoveryIO.class, CoreConfig.DISCOVERY_MODULES)) instanceof GenericElasticSearchIO);
     }
 
 }
