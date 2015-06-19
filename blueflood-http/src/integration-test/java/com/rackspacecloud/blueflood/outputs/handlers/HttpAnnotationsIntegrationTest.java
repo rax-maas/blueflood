876c884080e5f62d5a539a0b6f0af30c26b37edc
Tilottama Gaat <tilottama.gaat@rackspace.com>
2015-06-19 15:24:39 -0700

    Fixing tests

--- blueflood-http/src/integration-test/java/com/rackspacecloud/blueflood/outputs/handlers/HttpAnnotationsIntegrationTest.java
+++ blueflood-http/src/integration-test/java/com/rackspacecloud/blueflood/outputs/handlers/HttpAnnotationsIntegrationTest.java
@@ -20,14 +20,12 @@ import org.apache.http.client.methods.HttpGet;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Map;
+import java.util.*;
 
 public class HttpAnnotationsIntegrationTest extends IntegrationTestBase {
     //A time stamp 2 days ago
     private final long baseMillis = Calendar.getInstance().getTimeInMillis() - 172800000;
-    private final String tenantId = "ac" + IntegrationTestBase.randString(8);
+    private final String tenantId = "540123";
     private static int queryPort;
     private static EventElasticSearchIO eventsIO;
     private static EsSetup esSetup;
@@ -76,7 +74,8 @@ public class HttpAnnotationsIntegrationTest extends IntegrationTestBase {
     private URI getAnnotationsQueryURI() throws URISyntaxException {
         URIBuilder builder = new URIBuilder().setScheme("http").setHost("127.0.0.1")
                 .setPort(queryPort).setPath("/v2.0/" + tenantId + "/events/get_data")
-                .setParameter(Event.tagsParameterName,"Tag");
+                .setParameter(Event.fromParameterName, String.valueOf(baseMillis - 86400000))
+                .setParameter(Event.untilParameterName,String.valueOf(baseMillis + (86400000*3)));
         return builder.build();
     }
 
@@ -93,9 +92,9 @@ public class HttpAnnotationsIntegrationTest extends IntegrationTestBase {
         for (int i=0; i<eventCount; i++) {
             Event event = new Event();
             event.setWhat(String.format("[%s] %s %d", tenant, "Event title sample", i));
-            event.setWhen(Calendar.getInstance().getTimeInMillis());
+            event.setWhen(Long.parseLong(String.valueOf(Calendar.getInstance().getTimeInMillis())));
             event.setData(String.format("[%s] %s %d", tenant, "Event data sample", i));
-            event.setTags(String.format("%s","Tag"));
+            event.setTags(String.format("[%s] %s %d",tenant,"Sample tag",i));
 
             eventList.add(event.toMap());
         }
