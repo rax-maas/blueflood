72d9e5d7dc056c9e90053f2c0bc9ab8cfe797e01
Tilottama Gaat <tilottama.gaat@rackspace.com>
2015-06-22 14:13:03 -0700

    Changed the query endpoint

--- blueflood-http/src/main/java/com/rackspacecloud/blueflood/outputs/handlers/HttpMetricDataQueryServer.java
+++ blueflood-http/src/main/java/com/rackspacecloud/blueflood/outputs/handlers/HttpMetricDataQueryServer.java
@@ -63,7 +63,7 @@ public class HttpMetricDataQueryServer {
         router.post("/v2.0/:tenantId/views", new HttpMultiRollupsQueryHandler());
         router.get("/v2.0/:tenantId/views/histograms/:metricName", new HttpHistogramQueryHandler());
         router.get("/v2.0/:tenantId/metrics/search", new HttpMetricsIndexHandler());
-        router.get("/v2.0/:tenantId/events/get_data", new HttpEventsQueryHandler(EventModuleLoader.getInstance()));
+        router.get("/v2.0/:tenantId/events/getEvents", new HttpEventsQueryHandler(EventModuleLoader.getInstance()));
 
         log.info("Starting metric data query server (HTTP) on port {}", this.httpQueryPort);
         ServerBootstrap server = new ServerBootstrap(
