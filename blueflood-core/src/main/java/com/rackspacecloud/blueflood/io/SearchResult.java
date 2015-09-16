a507a56ad6236dfb6c65d00c83bcfd3677636641
Vinny Ly <vinny.ly@rackspace.com>
2015-09-15 21:00:21 -0500

    support enum values in ElasticSearch - extract ElasticIO.Discovery inner class to standalone Discovery class - extract ElasticIO.ESFieldLabel enum to standalone enum class - add configs for enums elasticsearch module - add EnumElasticIO class - add EnumMetric class to be used in ElasticSearch (and later Cassandra) - modify SearchResult to store enum values field - modify HttpMetricsIndexHandler to check for include_enum_values - modify HttpMetricsIndexHandler to include enum values in json result

--- blueflood-core/src/main/java/com/rackspacecloud/blueflood/io/SearchResult.java
+++ blueflood-core/src/main/java/com/rackspacecloud/blueflood/io/SearchResult.java
@@ -1,37 +1,74 @@
 package com.rackspacecloud.blueflood.io;
 
 import com.rackspacecloud.blueflood.types.Locator;
+import java.util.ArrayList;
 
 public class SearchResult {
     private final Locator locator;
     private final String unit;
+    private final ArrayList<String> enumValues;
 
     public SearchResult(String tenantId, String name, String unit) {
         this.locator = Locator.createLocatorFromDbKey(String.format("%s.%s", tenantId, name));
         this.unit = unit;
+        this.enumValues = null;
+    }
+
+    public SearchResult(String tenantId, String name, String unit, ArrayList<String> enumValues) {
+        this.locator = Locator.createLocatorFromDbKey(String.format("%s.%s", tenantId, name));
+        this.unit = unit;
+        this.enumValues = enumValues;
     }
 
     public String getTenantId() {
         return locator.getTenantId();
     }
+
     public String getMetricName() {
         return locator.getMetricName();
     }
+
     public String getUnit() {
         return unit;
     }
+
+    public ArrayList<String> getEnumValues() {
+        return enumValues;
+    }
+
     @Override
     public String toString() {
-        return "SearchResult [tenantId=" + locator.getTenantId() + ", metricName=" + locator.getMetricName() + ", unit=" + unit + "]";
+        String s = "SearchResult [tenantId=" + locator.getTenantId() + ", metricName=" + locator.getMetricName() + ", unit=" + unit;
+
+        if (enumValues != null) {
+            String enumValuesString = "";
+            for (String enumValue : enumValues) {
+                if (enumValuesString != "") {
+                    enumValuesString += ",";
+                }
+                enumValuesString += enumValue.replace(",", "\\,");
+            }
+            s += ", enumValues=" + enumValuesString + "]";
+        }
+
+        s += "]";
+        return s;
     }
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result
-                + locator.hashCode();
+
+        // hash code for locator
+        result = prime * result + locator.hashCode();
+
+        // hash code for unit if not null
         result = prime * result + ((unit == null) ? 0 : unit.hashCode());
+
+        // hash code for enum values if not null
+        result = prime * result + ((enumValues == null) ? 0 : enumValues.hashCode());
+
         return result;
     }
 
@@ -54,6 +91,18 @@ public class SearchResult {
 
         if (unit != null) {
             result = result && unit.equals(other.unit);
+        }
+
+        ArrayList<String> otherEnumValues = other.getEnumValues();
+        if ((enumValues != null) || (otherEnumValues != null)){
+            boolean enumValuesEqual;
+            if (enumValues != null) {
+                enumValuesEqual = enumValues.equals(otherEnumValues);
+            }
+            else {
+                enumValuesEqual = otherEnumValues.equals(enumValues);
+            }
+            result = result && enumValuesEqual;
         }
 
         return result;
