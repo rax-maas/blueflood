d1e839e536368d91e77a83a4b8d739d1fbc3e5a7
Vinny Ly <vinny.ly@rackspace.com>
2015-10-05 20:49:37 -0500

    Changes from code review for EnumValidator - Remove executor and async reads in EnumValidator for enum values. - Make EnumValidator threads only from rollup of 5min granularity. - only do validation for metrics with enum values from CF

--- blueflood-core/src/main/java/com/rackspacecloud/blueflood/service/EnumValidator.java
+++ blueflood-core/src/main/java/com/rackspacecloud/blueflood/service/EnumValidator.java
@@ -18,7 +18,6 @@ package com.rackspacecloud.blueflood.service;
 
 import com.netflix.astyanax.connectionpool.exceptions.ConnectionException;
 import com.netflix.astyanax.model.ColumnList;
-import com.rackspacecloud.blueflood.concurrent.ThreadPoolBuilder;
 import com.rackspacecloud.blueflood.io.AstyanaxReader;
 import com.rackspacecloud.blueflood.io.AstyanaxWriter;
 import com.rackspacecloud.blueflood.io.DiscoveryIO;
@@ -30,15 +29,12 @@ import com.rackspacecloud.blueflood.types.PreaggregatedMetric;
 import com.rackspacecloud.blueflood.utils.ModuleLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.*;
-import java.util.concurrent.*;
 
 /** This class handles collecting enum values for specific metrics, via their locators, and checking whether
     a uniqueness count of the enum values have reached a certain threshold.  If it has, the class mark the metric
-    as bad by inserting its locator into the metrics_excess_enums table.  If it hasn't reached the threshold, then the class
-    will create/update the elastic search "enums" index for the metric, which will contain a list of all unique
-    enum values that has been ingested for it.
+    as bad by inserting its locator into the proper cassandra column family.  If it hasn't reached the threshold, then it
+    will create or update the elasticsearch "enums" index for the metric.
 **/
 public class EnumValidator implements Runnable {
 
@@ -46,112 +42,95 @@ public class EnumValidator implements Runnable {
     private static final Configuration config = Configuration.getInstance();
     private static final DiscoveryIO discoveryIO = (DiscoveryIO) ModuleLoader.getInstance(DiscoveryIO.class, CoreConfig.ENUMS_DISCOVERY_MODULES);
     private static final int ENUM_UNIQUE_VALUES_THRESHOLD = config.getIntegerProperty(CoreConfig.ENUM_UNIQUE_VALUES_THRESHOLD);
-    private static final int ENUM_VALIDATOR_THREADS = Configuration.getInstance().getIntegerProperty(CoreConfig.ENUM_VALIDATOR_THREADS);
-
-    HashSet<Locator> locators;
+    private Set<Locator> locators;
 
     public EnumValidator(Set<Locator> locators) {
-        this.locators = (HashSet<Locator>) locators;
+        this.locators = locators;
     }
 
     @Override
     public void run() {
 
-        ExecutorService taskExecutor = new ThreadPoolBuilder().withUnboundedQueue()
-                .withCorePoolSize(ENUM_VALIDATOR_THREADS)
-                .withMaxPoolSize(ENUM_VALIDATOR_THREADS)
-                .withName("Enum Validator Task Executor")
-                .build();
+        if (locators == null) return;
+        Map<Locator, ColumnList<Long>> cfMetrics = AstyanaxReader.getInstance().getEnumHashMappings(new ArrayList(locators));
 
-        for (final Locator locator : locators) {
-            // start async read for enum values of locator from cassandra
-            Future<ColumnList<Long>> enumHashValuesFuture = taskExecutor.submit(new Callable() {
-                @Override
-                public ColumnList<Long> call() throws Exception {
-                    return AstyanaxReader.getInstance().getColumnsFromEnumCF(locator);
-                }
-
-            });
-
-            // start async read for enum values of locator from elastic search
-            Future<List<SearchResult>> esEnumValuesFuture = taskExecutor.submit(new Callable() {
-                @Override
-                public List<SearchResult> call() throws Exception {
-                    return discoveryIO.search(locator.getTenantId(), locator.getMetricName());
-                }
-            });
-
-
-            Map<Long, String> enumStringValues = null;
-            List<SearchResult> esEnumValues = null;
+        for (final Locator locator : cfMetrics.keySet()) {
+            // for each locator from CF results, get enum values
             try {
                 // values from cassandra db
-                ColumnList<Long> enumHashValues = enumHashValuesFuture.get();
-                if ((enumHashValues != null) && (enumHashValues.size() > 0)) {
-                    enumStringValues = AstyanaxReader.getInstance().getEnumValueFromHash(enumHashValues);
-                    log.debug(String.format("Enum values for metric %s: %s", locator.toString(), enumStringValues.toString()));
+                Map<Long, String> enumStringValuesFromCF = null;
+                ColumnList<Long> enumHashValuesFromCF = cfMetrics.get(locator);
+                if ((enumHashValuesFromCF != null) && (enumHashValuesFromCF.size() > 0)) {
+                    enumStringValuesFromCF = AstyanaxReader.getInstance().getEnumValueFromHashes(enumHashValuesFromCF);
                 }
                 else {
                     log.debug(String.format("No enum values found for metric %s", locator.toString()));
+                    // metric has no enum values, skip processing this locator and proceed to next one
+                    continue;
                 }
 
-                // values from elasticsearch
-                esEnumValues = esEnumValuesFuture.get();
+                // convert enum values to array list
+                ArrayList<String> currentEnumValues = null;
+                if ((enumStringValuesFromCF != null) && (enumStringValuesFromCF.size() > 0)) {
+                    currentEnumValues = new ArrayList<String>(enumStringValuesFromCF.values());
+                }
 
-                validateThresholdAndIndex(locator, enumStringValues, esEnumValues);
+                // validate enum values count and write to index or bad metric
+                validateThresholdAndWrite(locator, currentEnumValues);
 
-            } catch (InterruptedException e) {
-                log.error("Interrupted Exception during query of Enum metrics in EnumValidator", e);
-            } catch (ExecutionException e) {
-                log.error("Execution Exception during query of Enum metrics in EnumValidator", e);
-            }
-            finally {
-                taskExecutor.shutdown();
+            } catch (Exception e) {
+                log.error(String.format("Exception validating locator %s: %s", locator.toString(), e.getMessage()), e);
             }
         }
     }
 
-    private void validateThresholdAndIndex(Locator locator, Map<Long, String> enumStringValues, List<SearchResult> esEnumValues) {
+    private void validateThresholdAndWrite(Locator locator, ArrayList<String> currentEnumValues) {
+        // check if count of current enum values for the metric exceed a configurable threshold number
+        log.debug(String.format("EnumValidator validating locator %s", locator.toString()));
 
-        if ((enumStringValues != null) && (enumStringValues.size() > ENUM_UNIQUE_VALUES_THRESHOLD)) {
-            // if values_from_cassandra.count > threshold,
+        // if exceeded, mark metric as bad, else index enum values in elasticsearch
+        if ((currentEnumValues != null) && (currentEnumValues.size() > ENUM_UNIQUE_VALUES_THRESHOLD)) {
+            // count of current enum values of metric exceeded threshold, bad metric
             // write locator to bad metric table
             try {
                 AstyanaxWriter.getInstance().writeExcessEnumMetric(locator);
             } catch (ConnectionException e) {
-                log.error("Failed to write bad metric {} to table; {}", locator.toString(), e.getMessage());
+                log.error(String.format("Exception writing bad metric %s: %s", locator.toString(), e.getMessage()), e);
             }
         }
         else {
-            // create or update index of metric with enum values if cassandra and elasticsearch values are different
-            // get arraylist of cassandra enum values
-            ArrayList<String> dbValues = null;
-            if ((enumStringValues != null) && (enumStringValues.size() > 0)) {
-                dbValues = new ArrayList<String>(enumStringValues.values());
+            // not bad metric, create or update enums index of metric in elasticsearch if different from cassandra
+            // search for metric from elastic search
+            List<SearchResult> esSearchResult = null;
+            try {
+                esSearchResult = discoveryIO.search(locator.getTenantId(), locator.getMetricName());
+            }
+            catch (Exception e) {
+                log.error(String.format("Exception retrieving enum values from elasticsearch for %s: %s", locator.toString(), e.getMessage()), e);
             }
 
-            // get arraylist of elasticsearch enum values
-            ArrayList<String> esValues = null;
-            if ((esEnumValues != null) && (esEnumValues.get(0) != null)) {
-                esValues = esEnumValues.get(0).getEnumValues();
+            // get elasticsearch enum values from top search results of exact match
+            ArrayList<String> elasticsearchEnumValues = null;
+            if ((esSearchResult != null) && (esSearchResult.get(0) != null)) {
+                elasticsearchEnumValues = esSearchResult.get(0).getEnumValues();
             }
 
             // sort the two array lists of enum values
-            if (dbValues != null) Collections.sort(dbValues);
-            if (esValues != null) Collections.sort(esValues);
+            if (currentEnumValues != null) Collections.sort(currentEnumValues);
+            if (elasticsearchEnumValues != null) Collections.sort(elasticsearchEnumValues);
 
-            // verify equals
-            if (((dbValues != null) && (!dbValues.equals(esValues))) ||
-                ((dbValues == null) && (esValues != null) && (esValues.size() > 0)))
+            // compare two list of enum values
+            if (((currentEnumValues != null) && (!currentEnumValues.equals(elasticsearchEnumValues))) ||
+                ((currentEnumValues == null) && (elasticsearchEnumValues != null) && (elasticsearchEnumValues.size() > 0)))
             {
-                // not equal, create or update enums index in elastic search
-                BluefloodEnumRollup rollupWithEnumValues = createRollupWithEnumValues(dbValues);
+                // if not equal, create or update enums index in elastic search
+                BluefloodEnumRollup rollupWithEnumValues = createRollupWithEnumValues(currentEnumValues);
                 IMetric enumMetric = new PreaggregatedMetric(0, locator, null, rollupWithEnumValues);
                 try {
                     discoveryIO.insertDiscovery(enumMetric);
                 }
                 catch (Exception e) {
-                    log.error("Failed to write update elasticsearch enums index for {}; {}", locator.toString(), e.getMessage());
+                    log.error(String.format("Exception writing enums index to elasticsearch for %s: %s", locator.toString(), e.getMessage()), e);
                 }
             }
         }
