cdff7c98b6a088fd07e08989a26683fb463522be
Gaurav Bajaj <goru97@gmail.com>
2015-11-25 18:58:39 -0800

    added proxy server for grafana

--- contrib/garafana/grafana_1.x/blueflood/datasource.js
+++ contrib/garafana/grafana_1.x/blueflood/datasource.js
@@ -1,116 +1,123 @@
-'use strict';
-define([
-        'angular',
-        'lodash',
-        'kbn',
-        './bluefloodReposeWrapper'
-    ],
-    function (angular, _, kbn) {
-        //'use strict';
+    'use strict';
+    define([
+            'angular',
+            'lodash',
+            'kbn',
+            './bluefloodReposeWrapper'
+        ],
+        function (angular, _, kbn) {
+            //'use strict';
 
-        var module = angular.module('grafana.services');
+            var module = angular.module('grafana.services');
 
-        module.factory('BluefloodDatasource', function ($q, $http, templateSrv, ReposeAPI) {
+            module.factory('BluefloodDatasource', function ($q, $http, $location, templateSrv, ReposeAPI) {
 
-            /**
-             * Datasource initialization. Calls when you refresh page, add
-             * or modify datasource.
-             *
-             * @param {Object} datasource Grafana datasource object.
-             */
-            function BluefloodDatasource(datasource) {
-                this.name             = datasource.name;
-                this.type             = 'BluefloodDatasource';
-                this.url              = datasource.url;
-                this.username         = datasource.username;
-                this.apikey           = datasource.apikey;
-                this.tenantID         = datasource.tenantID;
-                this.identityURL      = "https://identity.api.rackspacecloud.com/v2.0/tokens";
+                /**
+                 * Datasource initialization. Calls when you refresh page, add
+                 * or modify datasource.
+                 *
+                 * @param {Object} datasource Grafana datasource object.
+                 */
+                function BluefloodDatasource(datasource) {
+                    this.name             = datasource.name;
+                    this.type             = 'BluefloodDatasource';
+                    this.username         = datasource.username;
+                    this.apikey           = datasource.apikey;
+                    this.tenantID         = datasource.tenantID;
+                    if(datasource.proxyEnabled) {
+                        this.url              = "http://"+$location.host()+":3000/";
+                        this.identityURL = "https://"+$location.host()+"/identity";
 
-                this.partials = datasource.partials || 'plugins/datasource/blueflood/partials';
-                this.annotationEditorSrc = this.partials + '/annotations.editor.html';
-
-                this.supportMetrics   = false;
-                this.supportAnnotations = true;
-
-                //Initialize Repose.
-                this.reposeAPI = new ReposeAPI(this.identityURL, this.username, this.apikey);
-            }
-
-            BluefloodDatasource.prototype.doAPIRequest = function(options, token) {
-                options.url   = this.url + '/v2.0/'+this.tenantID+options.url;
-                if(typeof token !== 'undefined'){
-                    options.headers = {
-                        'X-Auth-Token' : token.id
                     }
+                    else {
+                        this.identityURL = "https://identity.api.rackspacecloud.com/v2.0/tokens";
+                        this.url              = datasource.url;
+                    }
+
+                    this.partials = datasource.partials || 'plugins/datasource/blueflood/partials';
+                    this.annotationEditorSrc = this.partials + '/annotations.editor.html';
+
+                    this.supportMetrics   = false;
+                    this.supportAnnotations = true;
+
+                    //Initialize Repose.
+                    this.reposeAPI = new ReposeAPI(this.identityURL, this.username, this.apikey);
                 }
-                return $http.get(options.url, options);
-            };
 
-            /////////////////
-            // Annotations //
-            /////////////////
-
-            BluefloodDatasource.prototype.annotationQuery = function (annotation, rangeUnparsed) {
-
-                var tags = templateSrv.replace(annotation.tags);
-                return this.events({range: rangeUnparsed, tags: tags})
-                    .then(function (results) {
-                        var list = [];
-                        for (var i = 0; i < results.data.length; i++) {
-                            var e = results.data[i];
-
-                            list.push({
-                                annotation: annotation,
-                                time  :  e.when,
-                                title :  e.what,
-                                tags  :  e.tags,
-                                text  :  e.data
-                            });
+                BluefloodDatasource.prototype.doAPIRequest = function(options, token) {
+                    options.url   = this.url + '/v2.0/'+this.tenantID+options.url;
+                    if(typeof token !== 'undefined'){
+                        options.headers = {
+                            'X-Auth-Token' : token.id
                         }
-                        return list;
-                    });
-            };
-
-             BluefloodDatasource.prototype.events = function (options) {
-                try {
-                    var tags = '';
-                    if (options.tags) {
-                        tags = '&tags=' + options.tags;
                     }
+                    return $http.get(options.url, options);
+                };
 
-                    var d = $q.defer();
-                    this.doAPIRequest({
-                        method: 'GET',
-                        url: '/events/getEvents?from=' +this.translateTime(options.range.from)+ '&until=' +this.translateTime(options.range.to) + tags
-                    }, this.reposeAPI.getToken()).then(function (response) {
-                        if(response.status === 401){
-                            this.doAPIRequest({
-                                method: 'GET',
-                                url: '/events/getEvents?from=' +this.translateTime(options.range.from)+ '&until=' +this.translateTime(options.range.to) + tags
-                            }, this.reposeAPI.getIdentity()).then(function (response) {
-                                if(response.status/100 === 4 || response.status === 500){
-                                    alert("Error while connecting to Blueflood");
-                                }
+                /////////////////
+                // Annotations //
+                /////////////////
 
+                BluefloodDatasource.prototype.annotationQuery = function (annotation, rangeUnparsed) {
+
+                    var tags = templateSrv.replace(annotation.tags);
+                    return this.events({range: rangeUnparsed, tags: tags})
+                        .then(function (results) {
+                            var list = [];
+                            for (var i = 0; i < results.data.length; i++) {
+                                var e = results.data[i];
+
+                                list.push({
+                                    annotation: annotation,
+                                    time  :  e.when,
+                                    title :  e.what,
+                                    tags  :  e.tags,
+                                    text  :  e.data
+                                });
+                            }
+                            return list;
+                        });
+                };
+
+                BluefloodDatasource.prototype.events = function (options) {
+                    try {
+                        var tags = '';
+                        if (options.tags) {
+                            tags = '&tags=' + options.tags;
+                        }
+
+                        var d = $q.defer();
+                        this.doAPIRequest({
+                            method: 'GET',
+                            url: '/events/getEvents?from=' +this.translateTime(options.range.from)+ '&until=' +this.translateTime(options.range.to) + tags
+                        }, this.reposeAPI.getToken()).then(function (response) {
+                            if(response.status === 401){
+                                this.doAPIRequest({
+                                    method: 'GET',
+                                    url: '/events/getEvents?from=' +this.translateTime(options.range.from)+ '&until=' +this.translateTime(options.range.to) + tags
+                                }, this.reposeAPI.getIdentity()).then(function (response) {
+                                    if(response.status/100 === 4 || response.status === 500){
+                                        alert("Error while connecting to Blueflood");
+                                    }
+
+                                    d.resolve(response);
+
+                                });
+                            }
+                            else
                                 d.resolve(response);
+                        });
+                    }
+                    catch (err) {
+                        d.reject(err);
+                    }
+                    return d.promise;
+                };
 
-                            });
-                        }
-                        else
-                        d.resolve(response);
-                    });
-                }
-                catch (err) {
-                     d.reject(err);
-                }
-        return d.promise;
-            };
+                BluefloodDatasource.prototype.translateTime = function(date) {
+                    return kbn.parseDate(date).getTime();
+                };
 
-            BluefloodDatasource.prototype.translateTime = function(date) {
-              return kbn.parseDate(date).getTime();
-            };
-
-        return BluefloodDatasource;
+                return BluefloodDatasource;
+            });
         });
-    });
---
+++ contrib/garafana/grafana_1.x/middleman/middleman.js
@@ -0,0 +1,168 @@
+var express = require('express');
+var app = express();
+var Client = require('node-rest-client').Client;
+var client = new Client();
+var bodyParser = require('body-parser');
+var os = require('os');
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+
+var privateKey  = fs.readFileSync('server.key', 'utf8');
+var certificate = fs.readFileSync('server.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+
+app.options('/identity', function (req, res) {
+
+    res.set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST',
+        'Access-Control-Allow-Headers': 'X-Auth-Token, Accept, content-type, crossdomain',
+        'Access-Control-Max-Age': 1728000
+    })
+    res.sendStatus(200);
+
+});
+
+app.post('/identity', function (req, res) {
+
+    var args = {
+        data: req.body,
+        headers:{"Content-Type": "application/json"}
+    };
+
+    client.post("https://identity.api.rackspacecloud.com/v2.0/tokens", args, function(data,response) {
+        res.set({
+            'Access-Control-Allow-Origin': '*',
+            'content-type': 'application/json'
+        })
+        res.send(data);
+    });
+
+});
+
+app.options('/v2.0/:tenantId/events/getEvents', function (req, res) {
+
+    res.set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Headers': 'X-Auth-Token, Accept, content-type, crossdomain',
+        'Access-Control-Max-Age': 1728000
+    })
+    res.sendStatus(200);
+
+});
+
+app.get('/v2.0/:tenantId/events/getEvents', function (req, res) {
+
+    var args = {
+        data: req.body,
+        headers:{"Content-Type": "application/json"}
+    };
+
+    var reqURL = "http://iad.metrics.api.rackspacecloud.com/v2.0/"+req.params.tenantId+"events/getEvents?from="+
+        req.query.from+"&until="+req.query.until;
+
+    if (req.query.tags){
+       reqURL+="&tags="+req.query.tags
+    }
+
+    console.log(reqURL);
+    client.get(reqURL, args, function(data,response) {
+        res.set({
+            'Access-Control-Allow-Origin': '*',
+            'content-type': 'application/json'
+        })
+        res.send(data);
+    });
+
+});
+
+client.on('error',function(err){
+    console.error('Something went wrong on the client', err);
+});
+
+
+app.get('/', function (req, res) {
+
+    var interfaces = os.networkInterfaces();
+    var addresses = [];
+    for (var k in interfaces) {
+        for (var k2 in interfaces[k]) {
+            var address = interfaces[k][k2];
+            if (address.family === 'IPv4' && !address.internal) {
+                addresses.push(address.address);
+            }
+        }
+    }
+
+    res.set({
+        'Access-Control-Allow-Origin': '*',
+        'content-type': 'text/html'
+    })
+    res.send('<!DOCTYPE html><html>' +
+        '<head>' +
+        '<style>' +
+        '.background-image {' +
+        'position: fixed;' +
+        'left: 0;' +
+        'right: 0;' +
+        'z-index: 1;' +
+
+        'display: block;' +
+        'background-image: url(grafana_bck.png);' +
+        'width: 100%;' +
+        'height: 800px;' +
+
+        '-webkit-filter: blur(5px);' +
+        '-moz-filter: blur(10px);' +
+        '-o-filter: blur(5px);' +
+        '-ms-filter: blur(5px);' +
+        'filter: blur(5px);' +
+        '}' +
+
+        '.content {' +
+        'position: fixed;' +
+        'margin-left: auto;' +
+        'margin-right: auto;' +
+        'z-index: 9999;' +
+        'left: 45%;' +
+        'top: 50%;' +
+        '}' +
+
+        'a {' +
+        'display: block;' +
+        'width: 125px;' +
+        'height: 40px;' +
+        'text-decoration: none;' +
+        'background: #F5F500 ;' +
+        'padding: 10px;' +
+        'text-align: center;' +
+        'border-radius: 5px;' +
+        'color: #202020;' +
+        'font-weight: bold;' +
+        'font-family: Georgia' +
+        '}' +
+        '</style>' +
+        '</head>' +
+        '<body>' +
+        '<div class="background-image"></div>' +
+        '<div class="content">'+
+        '<a href=http://'+addresses[0]+'>Click here to start Grafana</a>' +
+        '</div>' +
+        '</body>' +
+        '</html>');
+});
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(3000);
+httpsServer.listen(443);
+
+console.log("HTTP server running on 3000");
+console.log("HTTPS server running on 443")
