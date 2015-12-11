2be1e3783872b42baa6d76ad4d06c1651c723ea1
Chandra Addala <chandra.addala@rackspace.com>
2015-12-11 16:33:34 -0600

    [CMD-714] Python script to clear errant enums.  - Removing pre-configured environments as they are company specific  - Environments are dynamic based on the ones configured in config.ini

--- contrib/enum_metrics/enum_metrics/config.py
+++ contrib/enum_metrics/enum_metrics/config.py
@@ -3,19 +3,24 @@ import os
 
 
 class Config:
+
+    parser = SafeConfigParser()
+    parser.read(os.path.join(os.path.dirname(__file__), '..', 'config.ini'))
+
     def __init__(self, env):
         self.env = env
-
-        self.parser = SafeConfigParser()
-        self.parser.read(os.path.join(os.path.dirname(__file__), '..', 'config.ini'))
 
         print self.parser.sections()
 
     def __get_value(self, config_name):
-        return self.parser.get(self.env, config_name)
+        return Config.parser.get(self.env, config_name)
 
     def get_cassandra_nodes(self):
         return self.__get_value('cassandra_nodes').split(",")
 
     def get_es_nodes(self):
         return self.__get_value('es_nodes').split(",")
+
+    @staticmethod
+    def get_environments():
+        return Config.parser.sections()
