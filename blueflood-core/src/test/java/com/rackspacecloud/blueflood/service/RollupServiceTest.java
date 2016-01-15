f4eb861fd583482c89d84a012cb5dbe2298cf65f
Richard Sartor <richard.sartor@rackspace.com>
2016-01-15 16:30:56 -0600

    Basic test of the RollupService.

---
+++ blueflood-core/src/test/java/com/rackspacecloud/blueflood/service/RollupServiceTest.java
@@ -0,0 +1,32 @@
+package com.rackspacecloud.blueflood.service;
+
+import com.rackspacecloud.blueflood.utils.Util;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class RollupServiceTest {
+
+    @Test
+    public void testRollupServiceWithDefaultConfigs() {
+
+        Configuration config = Configuration.getInstance();
+        final Collection<Integer> shards = Collections.unmodifiableCollection(
+                Util.parseShards(config.getStringProperty(CoreConfig.SHARDS)));
+        final String zkCluster = config.getStringProperty(CoreConfig.ZOOKEEPER_CLUSTER);
+        final ScheduleContext rollupContext = "NONE".equals(zkCluster) ?
+                new ScheduleContext(System.currentTimeMillis(), shards) :
+                new ScheduleContext(System.currentTimeMillis(), shards, zkCluster);
+
+        RollupService service = new RollupService(rollupContext);
+
+        Assert.assertNotNull(service);
+
+        service.run(null, 120000L);
+
+        Assert.assertTrue(true);
+    }
+}
