4ec3b1943a31a55406b293fb39e4811316c06a34
Shinta Smith <shinta.smith@RACKSPACE.COM>
2016-01-22 08:59:51 -0600

    fix errornous path in start up script

--- bin/blueflood-start.sh
+++ bin/blueflood-start.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-/usr/bin/java \
-        -Dblueflood.config=file:./demo/local/config/blueflood.conf \
-        -Dlog4j.configuration=file:./demo/local/config/blueflood-log4j.properties \
+
+#
+# This is a simple script that can be used to start blueflood service.
+# To run this, you must have built the blueflood package by running:
+#     mvn clean package -P all-modules
+#
+
+# directory where the script is located
+SCRIPTDIR=`pwd`/`dirname $0`  
+
+# top level source directory
+TOPDIR=$SCRIPTDIR/..
+
+JAVA=/usr/bin/java
+
+$JAVA \
+        -Dblueflood.config=file:///$TOPDIR/demo/local/config/blueflood.conf \
+        -Dlog4j.configuration=file:///$TOPDIR/demo/local/config/blueflood-log4j.properties \
         -Xms1G \
         -Xmx1G \
         -Dcom.sun.management.jmxremote.authenticate=false \
         -Dcom.sun.management.jmxremote.ssl=false \
         -Djava.rmi.server.hostname=localhost \
         -Dcom.sun.management.jmxremote.port=9180 \
-        -classpath ./blueflood-all/target/blueflood-all-*-jar-with-dependencies.jar com.rackspacecloud.blueflood.service.BluefloodServiceStarter 2>&1
+        -classpath $TOPDIR/blueflood-all/target/blueflood-all-*-jar-with-dependencies.jar com.rackspacecloud.blueflood.service.BluefloodServiceStarter 2>&1
