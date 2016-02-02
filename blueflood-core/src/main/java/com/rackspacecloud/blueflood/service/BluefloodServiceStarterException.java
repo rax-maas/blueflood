f697364778cbba9db4b13820a2f33b2f36e5c442
Richard Sartor <richard.sartor@rackspace.com>
2016-02-02 10:46:52 -0600

    Optionally include a cause of the exception.

--- blueflood-core/src/main/java/com/rackspacecloud/blueflood/service/BluefloodServiceStarterException.java
+++ blueflood-core/src/main/java/com/rackspacecloud/blueflood/service/BluefloodServiceStarterException.java
@@ -6,6 +6,11 @@ public class BluefloodServiceStarterException extends RuntimeException {
 
         this.status = status;
     }
+    public BluefloodServiceStarterException(int status, String message, Throwable cause) {
+        super(message, cause);
+
+        this.status = status;
+    }
 
     int status;
     public int getStatus() {
