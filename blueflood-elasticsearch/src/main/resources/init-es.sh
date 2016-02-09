b70ced0ace7046efaa2fd9b5b8db430ab29605cf
kaustav haldar <hi@kaustav.me>
2016-02-08 18:15:27 -0800

    use getopts

--- blueflood-elasticsearch/src/main/resources/init-es.sh
+++ blueflood-elasticsearch/src/main/resources/init-es.sh
@@ -6,10 +6,24 @@
 #WARNING: this script will destroy existing ES data and reset it to the proper init state
 # Example:
 #    Local ES: ./init-es.sh
-#    ES on OR: ./init-es.sh <url for OR:ES> <username> <password>
-ELASTICSEARCH_URL=${1:-"http://localhost:9200"}
-ES_USERNAME=${2:-}
-ES_PASSWD=${3:-}
+#    ES on OR: ./init-es.sh -u <url for OR:ES> -n <username> -p <password>
+
+# Set default ES URL
+ELASTICSEARCH_URL=http://localhost:9200
+
+usage() {
+    echo "Usage: $0 [-u <remote ES url:string>(default: localhost:9200)] [-n <username:string>] [-p <password:string>]" 1>&2; 
+    exit 1
+}
+
+while getopts "p:u:n:" o; do
+    case "${o}" in
+        p) ES_PASSWD=$OPTARG ;;
+        u) ELASTICSEARCH_URL=$OPTARG ;;
+        n) ES_USERNAME=$OPTARG ;;
+        *) usage ;;
+    esac
+done
 
 # Set a auth header for curl if username and passwd is supplied
 # Allows initing ES when ES is supplied by a SaaS provider like ObjectRocket
