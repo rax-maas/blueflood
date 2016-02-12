7828c45bc34e2e561566ab7f8c02aa312992221a
Richard Sartor <richard.sartor@rackspace.com>
2016-02-12 13:37:15 -0600

    Test update for all granularities.

--- blueflood-core/src/test/java/com/rackspacecloud/blueflood/service/ScheduleContextTest.java
+++ blueflood-core/src/test/java/com/rackspacecloud/blueflood/service/ScheduleContextTest.java
@@ -411,6 +411,65 @@ public class ScheduleContextTest {
     }
 
     @Test
+    public void testUpdateMarksSlotsDirtyAtAllGranularities() {
+
+        long now = 1234000L;
+        ScheduleContext ctx = new ScheduleContext(now, ringShards);
+        ShardStateManager mgr = ctx.getShardStateManager();
+        SlotKey slotkey5 = SlotKey.of(Granularity.MIN_5, 4, ringShards.get(0));
+        SlotKey slotkey20 = SlotKey.of(Granularity.MIN_20, 1, ringShards.get(0));
+        SlotKey slotkey60 = SlotKey.of(Granularity.MIN_60, 0, ringShards.get(0));
+        SlotKey slotkey240 = SlotKey.of(Granularity.MIN_240, 0, ringShards.get(0));
+        SlotKey slotkey1440 = SlotKey.of(Granularity.MIN_1440, 0, ringShards.get(0));
+
+        // precondition
+        Assert.assertEquals(0, ctx.getScheduledCount());
+        Assert.assertNull(mgr.getUpdateStamp(slotkey5));
+        Assert.assertNull(mgr.getUpdateStamp(slotkey20));
+        Assert.assertNull(mgr.getUpdateStamp(slotkey60));
+        Assert.assertNull(mgr.getUpdateStamp(slotkey240));
+        Assert.assertNull(mgr.getUpdateStamp(slotkey1440));
+
+        // when
+        ctx.update(now, ringShards.get(0));
+
+        // then
+        UpdateStamp stamp;
+
+        stamp = mgr.getUpdateStamp(slotkey5);
+        Assert.assertNotNull(stamp);
+        Assert.assertEquals(UpdateStamp.State.Active, stamp.getState());
+        Assert.assertEquals(now, stamp.getTimestamp());
+        Assert.assertTrue(stamp.isDirty());
+
+        stamp = mgr.getUpdateStamp(slotkey20);
+        Assert.assertNotNull(stamp);
+        Assert.assertEquals(UpdateStamp.State.Active, stamp.getState());
+        Assert.assertEquals(now, stamp.getTimestamp());
+        Assert.assertTrue(stamp.isDirty());
+
+        stamp = mgr.getUpdateStamp(slotkey60);
+        Assert.assertNotNull(stamp);
+        Assert.assertEquals(UpdateStamp.State.Active, stamp.getState());
+        Assert.assertEquals(now, stamp.getTimestamp());
+        Assert.assertTrue(stamp.isDirty());
+
+        stamp = mgr.getUpdateStamp(slotkey240);
+        Assert.assertNotNull(stamp);
+        Assert.assertEquals(UpdateStamp.State.Active, stamp.getState());
+        Assert.assertEquals(now, stamp.getTimestamp());
+        Assert.assertTrue(stamp.isDirty());
+
+        stamp = mgr.getUpdateStamp(slotkey1440);
+        Assert.assertNotNull(stamp);
+        Assert.assertEquals(UpdateStamp.State.Active, stamp.getState());
+        Assert.assertEquals(now, stamp.getTimestamp());
+        Assert.assertTrue(stamp.isDirty());
+
+        Assert.assertEquals(0, ctx.getScheduledCount());
+    }
+
+    @Test
     public void testScheduleSlotsOlderThanAddsToScheduledCount() {
 
         long now = 1234000L;
