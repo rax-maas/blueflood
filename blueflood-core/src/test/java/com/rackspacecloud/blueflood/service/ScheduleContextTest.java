87d835703450e33fd0ba1ff5635558351bf2f9f0
Richard Sartor <richard.sartor@rackspace.com>
2016-02-12 13:23:21 -0600

    Add another, slightly more complex test for scheduleSlotsOlderThan.

--- blueflood-core/src/test/java/com/rackspacecloud/blueflood/service/ScheduleContextTest.java
+++ blueflood-core/src/test/java/com/rackspacecloud/blueflood/service/ScheduleContextTest.java
@@ -415,7 +415,6 @@ public class ScheduleContextTest {
 
         long now = 1234000L;
         ScheduleContext ctx = new ScheduleContext(now, ringShards);
-        SlotKey slotkey = SlotKey.of(Granularity.MIN_5, 4, ringShards.get(0));
         ctx.update(now-2, ringShards.get(0));
 
         // precondition
@@ -427,4 +426,28 @@ public class ScheduleContextTest {
         // then
         Assert.assertEquals(1, ctx.getScheduledCount());
     }
+
+    @Test
+    public void testScheduleSlotsOlderThanSmallestGranularityOnlyAddsOneToScheduledCount() {
+
+        // here we update for a time just over five minutes ago. five minutes
+        // is the smallest granularity, so we might expect that this update
+        // would dirty both of the two most recent 5min slots. however, the
+        // ScheduleContext will only mark the oldest of them as dirty.
+
+        long now = 1234000L;
+        long fiveMinutes = 5 * 60 * 1000;
+        ScheduleContext ctx = new ScheduleContext(now, ringShards);
+        ctx.update(now-fiveMinutes-1, ringShards.get(0));
+
+        // precondition
+        Assert.assertEquals(0, ctx.getScheduledCount());
+
+        // when
+        ctx.scheduleSlotsOlderThan(fiveMinutes);
+
+        // then
+        Assert.assertEquals(1, ctx.getScheduledCount());
+    }
+
 }
