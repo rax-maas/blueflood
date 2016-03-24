cd9f21ede37657f4103d4995ddc7be306423c7ad
Richard Sartor <richard.sartor@rackspace.com>
2016-03-24 10:31:38 -0500

    Add a simple happy-path test for drainExecutionContext, and a hand-written mock class.

---
+++ blueflood-core/src/test/java/com/rackspacecloud/blueflood/service/LocatorFetchRunnableDrainExecutionContextTest.java
@@ -0,0 +1,115 @@
+package com.rackspacecloud.blueflood.service;
+
+import com.rackspacecloud.blueflood.io.AstyanaxReader;
+import com.rackspacecloud.blueflood.rollup.Granularity;
+import com.rackspacecloud.blueflood.rollup.SlotKey;
+import com.rackspacecloud.blueflood.threading.SizedExecutorService;
+import com.rackspacecloud.blueflood.types.Locator;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.RejectedExecutionException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+public class LocatorFetchRunnableDrainExecutionContextTest {
+
+
+    ScheduleContext scheduleCtx;
+    SlotKey destSlotKey;
+    ExecutorService rollupReadExecutor;
+    SizedExecutorService rollupWriteExecutor;
+    ExecutorService enumValidatorExecutor;
+    AstyanaxReader astyanaxReader;
+
+    StubbedLocatorFetchRunnable lfr;
+
+    RollupExecutionContext executionContext;
+    RollupBatchWriter rollupBatchWriter;
+
+    List<Locator> locators;
+
+    @Before
+    public void setUp() throws IOException {
+
+        Configuration.getInstance().init();
+
+        this.scheduleCtx = mock(ScheduleContext.class);
+        this.destSlotKey = SlotKey.of(Granularity.FULL, 0, 0);
+        this.rollupReadExecutor = mock(ExecutorService.class);
+        this.rollupWriteExecutor = mock(SizedExecutorService.class);
+        this.enumValidatorExecutor = mock(ExecutorService.class);
+        this.astyanaxReader = mock(AstyanaxReader.class);
+
+        this.lfr = new StubbedLocatorFetchRunnable(scheduleCtx, destSlotKey,
+                rollupReadExecutor, rollupWriteExecutor, enumValidatorExecutor,
+                astyanaxReader);
+
+        executionContext = mock(RollupExecutionContext.class);
+        rollupBatchWriter = mock(RollupBatchWriter.class);
+
+        locators = getTypicalLocators();
+
+        Configuration.getInstance().setProperty(CoreConfig.ENABLE_HISTOGRAMS,
+                "false");
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        Configuration.getInstance().init();
+    }
+
+    List<Locator> getTypicalLocators() {
+
+        final Locator locator1 =
+                Locator.createLocatorFromPathComponents("tenant1", "a", "b", "c");
+        final Locator locator2 =
+                Locator.createLocatorFromPathComponents("tenant2", "a", "b", "x");
+        final Locator locator3 =
+                Locator.createLocatorFromPathComponents("tenant3", "d", "e", "f");
+        final List<Locator> locators = new ArrayList<Locator>() {{
+            add(locator1);
+            add(locator2);
+            add(locator3);
+        }};
+        return locators;
+    }
+
+    @Test
+    public void drainExecutionContextAlreadyDoneReadingAndWriting() {
+
+        // given
+        when(executionContext.doneReading()).thenReturn(true);
+        when(executionContext.doneWriting()).thenReturn(true);
+
+        // when
+        lfr.drainExecutionContext(0, 0, executionContext, rollupBatchWriter);
+
+        // then
+        verify(executionContext, times(1)).doneReading();
+        verify(executionContext, times(1)).doneWriting();
+        verifyNoMoreInteractions(executionContext);
+        verifyZeroInteractions(rollupBatchWriter);
+        verify(scheduleCtx, times(1)).getCurrentTimeMillis();
+        verifyNoMoreInteractions(scheduleCtx);
+        verifyZeroInteractions(rollupReadExecutor);
+        verifyZeroInteractions(rollupWriteExecutor);
+        verifyZeroInteractions(enumValidatorExecutor);
+        verifyZeroInteractions(astyanaxReader);
+        assertTrue(lfr.wasMethodCalled(StubbedLocatorFetchRunnable.MethodToken.finishExecution)); // verify(lfr, times(1)).finishExecution(...)
+        assertEquals(1, lfr.getInteractions().size());  // verifyNoMoreInteractions(lfr)
+    }
+}
