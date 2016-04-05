2a6aeb0fff6bb5f5dc6d0ef780a62f28dce3fdd0
Richard Sartor <richard.sartor@rackspace.com>
2016-04-05 16:48:56 -0500

    Test cases that do and do not trigger the batching functionality.

--- blueflood-core/src/test/java/com/rackspacecloud/blueflood/service/RollupBatchWriterTest.java
+++ blueflood-core/src/test/java/com/rackspacecloud/blueflood/service/RollupBatchWriterTest.java
@@ -1,6 +1,7 @@
 package com.rackspacecloud.blueflood.service;
 
 import org.junit.Test;
+import org.mockito.Matchers;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
@@ -30,5 +31,124 @@ public class RollupBatchWriterTest {
         verifyNoMoreInteractions(ctx);
 
         verifyZeroInteractions(srwc);
+    }
+
+    @Test
+    public void enqueuingLessThanMinSizeDoesNotTriggerBatching() {
+
+        // given
+        ThreadPoolExecutor executor = mock(ThreadPoolExecutor.class);
+        RollupExecutionContext ctx = mock(RollupExecutionContext.class);
+        SingleRollupWriteContext srwc1 = mock(SingleRollupWriteContext.class);
+        SingleRollupWriteContext srwc2 = mock(SingleRollupWriteContext.class);
+        SingleRollupWriteContext srwc3 = mock(SingleRollupWriteContext.class);
+        SingleRollupWriteContext srwc4 = mock(SingleRollupWriteContext.class);
+        // ROLLUP_BATCH_MIN_SIZE default value is 5
+
+        RollupBatchWriter rbw = new RollupBatchWriter(executor, ctx);
+
+        // when
+        rbw.enqueueRollupForWrite(srwc1);
+        rbw.enqueueRollupForWrite(srwc2);
+        rbw.enqueueRollupForWrite(srwc3);
+        rbw.enqueueRollupForWrite(srwc4);
+
+        // then
+        verify(ctx, times(4)).incrementWriteCounter();
+        verifyNoMoreInteractions(ctx);
+
+        verifyZeroInteractions(srwc1);
+        verifyZeroInteractions(srwc2);
+        verifyZeroInteractions(srwc3);
+        verifyZeroInteractions(srwc4);
+
+        verifyZeroInteractions(executor);
+    }
+
+    @Test
+    public void enqueuingMinSizeTriggersCheckOnExecutor() {
+
+        // given
+        ThreadPoolExecutor executor = mock(ThreadPoolExecutor.class);
+        // if active count == pool size, the RollupBatchWriter will think the
+        // thread pool is saturated, and not drain
+        doReturn(1).when(executor).getActiveCount();
+        doReturn(1).when(executor).getPoolSize();
+
+        RollupExecutionContext ctx = mock(RollupExecutionContext.class);
+        SingleRollupWriteContext srwc1 = mock(SingleRollupWriteContext.class);
+        SingleRollupWriteContext srwc2 = mock(SingleRollupWriteContext.class);
+        SingleRollupWriteContext srwc3 = mock(SingleRollupWriteContext.class);
+        SingleRollupWriteContext srwc4 = mock(SingleRollupWriteContext.class);
+        SingleRollupWriteContext srwc5 = mock(SingleRollupWriteContext.class);
+        // ROLLUP_BATCH_MIN_SIZE default value is 5
+
+        RollupBatchWriter rbw = new RollupBatchWriter(executor, ctx);
+
+        // when
+        rbw.enqueueRollupForWrite(srwc1);
+        rbw.enqueueRollupForWrite(srwc2);
+        rbw.enqueueRollupForWrite(srwc3);
+        rbw.enqueueRollupForWrite(srwc4);
+        rbw.enqueueRollupForWrite(srwc5);
+
+        // then
+        verify(ctx, times(5)).incrementWriteCounter();
+        verifyNoMoreInteractions(ctx);
+
+        verifyZeroInteractions(srwc1);
+        verifyZeroInteractions(srwc2);
+        verifyZeroInteractions(srwc3);
+        verifyZeroInteractions(srwc4);
+        verifyZeroInteractions(srwc5);
+
+        // if the queue size >= min size, then the executor will be queried
+        verify(executor).getActiveCount();
+        verify(executor).getPoolSize();
+        verifyNoMoreInteractions(executor);
+    }
+
+    @Test
+    public void enqueuingMinSizeAndThreadPoolNotSaturatedTriggersBatching() {
+
+        // given
+        ThreadPoolExecutor executor = mock(ThreadPoolExecutor.class);
+        // if active count == pool size, the RollupBatchWriter will think the
+        // thread pool is saturated, and not drain
+        doReturn(0).when(executor).getActiveCount();
+        doReturn(1).when(executor).getPoolSize();
+
+        RollupExecutionContext ctx = mock(RollupExecutionContext.class);
+        SingleRollupWriteContext srwc1 = mock(SingleRollupWriteContext.class);
+        SingleRollupWriteContext srwc2 = mock(SingleRollupWriteContext.class);
+        SingleRollupWriteContext srwc3 = mock(SingleRollupWriteContext.class);
+        SingleRollupWriteContext srwc4 = mock(SingleRollupWriteContext.class);
+        SingleRollupWriteContext srwc5 = mock(SingleRollupWriteContext.class);
+        // ROLLUP_BATCH_MIN_SIZE default value is 5
+
+        RollupBatchWriter rbw = new RollupBatchWriter(executor, ctx);
+
+        // when
+        rbw.enqueueRollupForWrite(srwc1);
+        rbw.enqueueRollupForWrite(srwc2);
+        rbw.enqueueRollupForWrite(srwc3);
+        rbw.enqueueRollupForWrite(srwc4);
+        rbw.enqueueRollupForWrite(srwc5);
+
+        // then
+        verify(ctx, times(5)).incrementWriteCounter();
+        verifyNoMoreInteractions(ctx);
+
+        verifyZeroInteractions(srwc1);
+        verifyZeroInteractions(srwc2);
+        verifyZeroInteractions(srwc3);
+        verifyZeroInteractions(srwc4);
+        verifyZeroInteractions(srwc5);
+
+        // if the queue size >= min size, then the executor will be queried
+        verify(executor).getActiveCount();
+        verify(executor).getPoolSize();
+        verify(executor).execute(Matchers.<Runnable>any());
+        verifyNoMoreInteractions(executor);
     }
 }
