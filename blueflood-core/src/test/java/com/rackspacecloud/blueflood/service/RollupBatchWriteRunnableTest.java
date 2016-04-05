312b422081dee8484cdcecd36acecd2c82a3047a
Richard Sartor <richard.sartor@rackspace.com>
2016-04-05 16:08:17 -0500

    Test exception handling.

--- blueflood-core/src/test/java/com/rackspacecloud/blueflood/service/RollupBatchWriteRunnableTest.java
+++ blueflood-core/src/test/java/com/rackspacecloud/blueflood/service/RollupBatchWriteRunnableTest.java
@@ -7,12 +7,12 @@ import org.mockito.Matchers;
 
 import java.util.ArrayList;
 
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertSame;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyLong;
 import static org.mockito.Matchers.argThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.*;
 
 public class RollupBatchWriteRunnableTest {
 
@@ -34,5 +34,58 @@ public class RollupBatchWriteRunnableTest {
         verifyNoMoreInteractions(writer);
         verify(ctx).decrementWriteCounter(anyLong());
         verifyNoMoreInteractions(ctx);
+    }
+
+    @Test
+    public void connectionExceptionMarksUnsuccessful() throws ConnectionException {
+
+        // given
+        ArrayList<SingleRollupWriteContext> wcs = new ArrayList<SingleRollupWriteContext>();
+        ArrayList<SingleRollupWriteContext> wcs2 = new ArrayList<SingleRollupWriteContext>();
+        RollupExecutionContext ctx = mock(RollupExecutionContext.class);
+        AstyanaxWriter writer = mock(AstyanaxWriter.class);
+        Throwable cause = new ConnectionException("exception for testing purposes") { };
+        doThrow(cause).when(writer).insertRollups(
+                Matchers.<ArrayList<SingleRollupWriteContext>>any());
+        RollupBatchWriteRunnable rbwr = new RollupBatchWriteRunnable(wcs, ctx, writer);
+
+        // when
+        rbwr.run();
+
+        // then
+        verify(writer).insertRollups(Matchers.<ArrayList<SingleRollupWriteContext>>any());
+        verifyNoMoreInteractions(writer);
+        verify(ctx).markUnsuccessful(Matchers.<Throwable>any());
+        verify(ctx).decrementWriteCounter(anyLong());
+        verifyNoMoreInteractions(ctx);
+    }
+
+    @Test
+    public void otherExceptionBreaksEverything() throws ConnectionException {
+
+        // given
+        ArrayList<SingleRollupWriteContext> wcs = new ArrayList<SingleRollupWriteContext>();
+        ArrayList<SingleRollupWriteContext> wcs2 = new ArrayList<SingleRollupWriteContext>();
+        RollupExecutionContext ctx = mock(RollupExecutionContext.class);
+        AstyanaxWriter writer = mock(AstyanaxWriter.class);
+        Throwable cause = new UnsupportedOperationException("exception for testing purposes");
+        doThrow(cause).when(writer).insertRollups(
+                Matchers.<ArrayList<SingleRollupWriteContext>>any());
+        RollupBatchWriteRunnable rbwr = new RollupBatchWriteRunnable(wcs, ctx, writer);
+
+        // when
+        Throwable caught = null;
+        try {
+            rbwr.run();
+        } catch (Throwable t) {
+            caught = t;
+        }
+
+        // then
+        assertNotNull(caught);
+        assertSame(cause, caught);
+        verify(writer).insertRollups(Matchers.<ArrayList<SingleRollupWriteContext>>any());
+        verifyNoMoreInteractions(writer);
+        verifyZeroInteractions(ctx);
     }
 }
