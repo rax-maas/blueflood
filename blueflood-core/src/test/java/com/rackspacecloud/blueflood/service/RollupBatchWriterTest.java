5083e5208180f9b8ee7b0ea97e9e6942395b07b7
Richard Sartor <richard.sartor@rackspace.com>
2016-04-06 09:46:18 -0500

    Extract common setup into a @Before method.

--- blueflood-core/src/test/java/com/rackspacecloud/blueflood/service/RollupBatchWriterTest.java
+++ blueflood-core/src/test/java/com/rackspacecloud/blueflood/service/RollupBatchWriterTest.java
@@ -1,27 +1,33 @@
 package com.rackspacecloud.blueflood.service;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Matchers;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 import static org.mockito.Mockito.*;
 
 public class RollupBatchWriterTest {
 
+    ThreadPoolExecutor executor;
+    RollupExecutionContext ctx;
+
+    RollupBatchWriter rbw;
+
+    @Before
+    public void setUp() {
+
+        executor = mock(ThreadPoolExecutor.class);
+        ctx = mock(RollupExecutionContext.class);
+        rbw = new RollupBatchWriter(executor, ctx);
+    }
+
     @Test
     public void enqueueIncrementsWriterCounter() {
 
         // given
-        BlockingQueue<Runnable> queue = new ArrayBlockingQueue<Runnable>(5);
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(2, 4, 1000, TimeUnit.MILLISECONDS, queue);
-        RollupExecutionContext ctx = mock(RollupExecutionContext.class);
         SingleRollupWriteContext srwc = mock(SingleRollupWriteContext.class);
-
-        RollupBatchWriter rbw = new RollupBatchWriter(executor, ctx);
 
         // when
         rbw.enqueueRollupForWrite(srwc);
@@ -37,15 +43,11 @@ public class RollupBatchWriterTest {
     public void enqueuingLessThanMinSizeDoesNotTriggerBatching() {
 
         // given
-        ThreadPoolExecutor executor = mock(ThreadPoolExecutor.class);
-        RollupExecutionContext ctx = mock(RollupExecutionContext.class);
         SingleRollupWriteContext srwc1 = mock(SingleRollupWriteContext.class);
         SingleRollupWriteContext srwc2 = mock(SingleRollupWriteContext.class);
         SingleRollupWriteContext srwc3 = mock(SingleRollupWriteContext.class);
         SingleRollupWriteContext srwc4 = mock(SingleRollupWriteContext.class);
         // ROLLUP_BATCH_MIN_SIZE default value is 5
-
-        RollupBatchWriter rbw = new RollupBatchWriter(executor, ctx);
 
         // when
         rbw.enqueueRollupForWrite(srwc1);
@@ -69,21 +71,18 @@ public class RollupBatchWriterTest {
     public void enqueuingMinSizeTriggersCheckOnExecutor() {
 
         // given
-        ThreadPoolExecutor executor = mock(ThreadPoolExecutor.class);
+
         // if active count == pool size, the RollupBatchWriter will think the
         // thread pool is saturated, and not drain
         doReturn(1).when(executor).getActiveCount();
         doReturn(1).when(executor).getPoolSize();
 
-        RollupExecutionContext ctx = mock(RollupExecutionContext.class);
         SingleRollupWriteContext srwc1 = mock(SingleRollupWriteContext.class);
         SingleRollupWriteContext srwc2 = mock(SingleRollupWriteContext.class);
         SingleRollupWriteContext srwc3 = mock(SingleRollupWriteContext.class);
         SingleRollupWriteContext srwc4 = mock(SingleRollupWriteContext.class);
         SingleRollupWriteContext srwc5 = mock(SingleRollupWriteContext.class);
         // ROLLUP_BATCH_MIN_SIZE default value is 5
-
-        RollupBatchWriter rbw = new RollupBatchWriter(executor, ctx);
 
         // when
         rbw.enqueueRollupForWrite(srwc1);
@@ -112,21 +111,17 @@ public class RollupBatchWriterTest {
     public void enqueuingMinSizeAndThreadPoolNotSaturatedTriggersBatching() {
 
         // given
-        ThreadPoolExecutor executor = mock(ThreadPoolExecutor.class);
         // if active count < pool size, the RollupBatchWriter will think the
         // thread pool is NOT saturated, and start batching
         doReturn(0).when(executor).getActiveCount();
         doReturn(1).when(executor).getPoolSize();
 
-        RollupExecutionContext ctx = mock(RollupExecutionContext.class);
         SingleRollupWriteContext srwc1 = mock(SingleRollupWriteContext.class);
         SingleRollupWriteContext srwc2 = mock(SingleRollupWriteContext.class);
         SingleRollupWriteContext srwc3 = mock(SingleRollupWriteContext.class);
         SingleRollupWriteContext srwc4 = mock(SingleRollupWriteContext.class);
         SingleRollupWriteContext srwc5 = mock(SingleRollupWriteContext.class);
         // ROLLUP_BATCH_MIN_SIZE default value is 5
-
-        RollupBatchWriter rbw = new RollupBatchWriter(executor, ctx);
 
         // when
         rbw.enqueueRollupForWrite(srwc1);
@@ -156,21 +151,17 @@ public class RollupBatchWriterTest {
     public void enqueuingMaxSizeTriggersBatching() {
 
         // given
-        ThreadPoolExecutor executor = mock(ThreadPoolExecutor.class);
         // if active count == pool size, the RollupBatchWriter will think the
         // thread pool is saturated, and not drain
         doReturn(1).when(executor).getActiveCount();
         doReturn(1).when(executor).getPoolSize();
 
-        RollupExecutionContext ctx = mock(RollupExecutionContext.class);
         SingleRollupWriteContext[] srwcs = new SingleRollupWriteContext[100];
         int i;
         for (i = 0; i < 100; i++) {
             srwcs[i] = mock(SingleRollupWriteContext.class);
         }
         // ROLLUP_BATCH_MAX_SIZE default value is 100
-
-        RollupBatchWriter rbw = new RollupBatchWriter(executor, ctx);
 
         // when
         for (i = 0; i < 100; i++) {
@@ -195,11 +186,6 @@ public class RollupBatchWriterTest {
     @Test
     public void drainBatchWithNoItemsDoesNotTriggerBatching() {
 
-        // given
-        ThreadPoolExecutor executor = mock(ThreadPoolExecutor.class);
-        RollupExecutionContext ctx = mock(RollupExecutionContext.class);
-        RollupBatchWriter rbw = new RollupBatchWriter(executor, ctx);
-
         // when
         rbw.drainBatch();
 
@@ -213,9 +199,6 @@ public class RollupBatchWriterTest {
     public void drainBatchWithSingleItemTriggersBatching() {
 
         // given
-        ThreadPoolExecutor executor = mock(ThreadPoolExecutor.class);
-        RollupExecutionContext ctx = mock(RollupExecutionContext.class);
-        RollupBatchWriter rbw = new RollupBatchWriter(executor, ctx);
         SingleRollupWriteContext srwc = mock(SingleRollupWriteContext.class);
         rbw.enqueueRollupForWrite(srwc);
 
