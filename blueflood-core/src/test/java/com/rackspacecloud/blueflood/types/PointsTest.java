89555eab3b8908e9fc6d67e883bda3da8f00b2a0
Richard Sartor <richard.sartor@rackspace.com>
2016-04-07 09:27:51 -0500

    Add tests for duplicate timestamps.

--- blueflood-core/src/test/java/com/rackspacecloud/blueflood/types/PointsTest.java
+++ blueflood-core/src/test/java/com/rackspacecloud/blueflood/types/PointsTest.java
@@ -64,4 +64,52 @@ public class PointsTest {
         // then
         assertSame(SimpleNumber.class, actual);
     }
+
+    // adding point with duplicate timestamp replaces previous, e.g. doesn't increase count
+    @Test
+    public void addWithDuplicateTimestampDoesNotIncreaseCount() {
+
+        // given
+        Points<SimpleNumber> points = new Points<SimpleNumber>();
+
+        SimpleNumber item = new SimpleNumber(42);
+        SimpleNumber item2 = new SimpleNumber(43);
+        long timestamp = 1234L;
+        Points.Point<SimpleNumber> point = new Points.Point<SimpleNumber>(timestamp, item);
+        Points.Point<SimpleNumber> point2 = new Points.Point<SimpleNumber>(timestamp, item2);
+        points.add(point);
+
+        // precondition
+        assertEquals(1, points.getPoints().size());
+
+        // when
+        points.add(point2);
+
+        // then
+        assertFalse(points.isEmpty());
+        assertEquals(1, points.getPoints().size());
+    }
+
+    @Test
+    public void addWithDuplicateTimestampReplacesPrevious() {
+
+        // given
+        Points<SimpleNumber> points = new Points<SimpleNumber>();
+
+        SimpleNumber item = new SimpleNumber(42);
+        SimpleNumber item2 = new SimpleNumber(43);
+        long timestamp = 1234L;
+        Points.Point<SimpleNumber> point = new Points.Point<SimpleNumber>(timestamp, item);
+        Points.Point<SimpleNumber> point2 = new Points.Point<SimpleNumber>(timestamp, item2);
+        points.add(point);
+
+        // precondition
+        assertSame(item, points.getPoints().get(timestamp).getData());
+
+        // when
+        points.add(point2);
+
+        // then
+        assertSame(item2, points.getPoints().get(timestamp).getData());
+    }
 }
