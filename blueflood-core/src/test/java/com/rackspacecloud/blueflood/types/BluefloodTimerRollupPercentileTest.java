eb9f79d746ede2c7559c55bfa4523c4bd5bac3e6
Richard Sartor <richard.sartor@rackspace.com>
2016-04-22 11:41:26 -0500

    Test the equals method.

--- blueflood-core/src/test/java/com/rackspacecloud/blueflood/types/BluefloodTimerRollupPercentileTest.java
+++ blueflood-core/src/test/java/com/rackspacecloud/blueflood/types/BluefloodTimerRollupPercentileTest.java
@@ -5,8 +5,7 @@ import org.junit.Test;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class BluefloodTimerRollupPercentileTest {
 
@@ -140,5 +139,66 @@ public class BluefloodTimerRollupPercentileTest {
         BluefloodTimerRollup.Percentile value = new BluefloodTimerRollup.Percentile(123.45d);
         // then
         assertEquals(123.45d, (Double)value.getMean(), 0.00001d);
+    }
+
+    @Test
+    public void equalsWithNullReturnsFalse() {
+        // given
+        BluefloodTimerRollup.Percentile value = new BluefloodTimerRollup.Percentile((Number)123L);
+        // expect
+        assertFalse(value.equals(null));
+    }
+
+    @Test
+    public void equalsWithNonPercentileReturnsFalse() {
+        // given
+        BluefloodTimerRollup.Percentile value = new BluefloodTimerRollup.Percentile((Number)123L);
+        // expect
+        assertFalse(value.equals(new Object()));
+    }
+
+    @Test
+    public void equalsWithSelfReturnsTrue() {
+        // given
+        BluefloodTimerRollup.Percentile value = new BluefloodTimerRollup.Percentile((Number)123L);
+        // expect
+        assertTrue(value.equals(value));
+    }
+
+    @Test
+    public void equalsWithOtherRollupWithSameLongValueReturnsTrue() {
+        // given
+        BluefloodTimerRollup.Percentile a = new BluefloodTimerRollup.Percentile((Number)123L);
+        BluefloodTimerRollup.Percentile b = new BluefloodTimerRollup.Percentile((Number)123L);
+        // expect
+        assertTrue(a.equals(b));
+    }
+
+    @Test
+    public void equalsWithOtherRollupWithSameDoubleValueReturnsTrue() {
+        // given
+        BluefloodTimerRollup.Percentile a = new BluefloodTimerRollup.Percentile((Number)123.0d);
+        BluefloodTimerRollup.Percentile b = new BluefloodTimerRollup.Percentile((Number)123.0d);
+        // expect
+        assertTrue(a.equals(b));
+    }
+
+    @Test
+    public void equalsWithOtherRollupWithSlightlyDifferentDoubleValueReturnsTrue() {
+        // given
+        BluefloodTimerRollup.Percentile a = new BluefloodTimerRollup.Percentile((Number)123.0d);
+        BluefloodTimerRollup.Percentile b = new BluefloodTimerRollup.Percentile((Number)123.00000000000001d); // least-significant bit different
+        // expect
+        assertFalse(a.equals(b));
+    }
+
+    @Test
+    public void equalsWithDoubleAndLongOfSameValueReturnsFalse() {
+        // given
+        BluefloodTimerRollup.Percentile a = new BluefloodTimerRollup.Percentile((Number)123L);
+        BluefloodTimerRollup.Percentile b = new BluefloodTimerRollup.Percentile((Number)123.0d);
+        // expect
+        assertTrue(123L == 123.0d);
+        assertFalse(a.equals(b));
     }
 }
