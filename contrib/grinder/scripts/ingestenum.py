ce1c592a00ed99043019083230f5b6cd44b04038
Shinta Smith <shinta.smith@rackspace.com>
2016-09-30 10:45:16 -0500

    fix broken grinder

--- contrib/grinder/scripts/ingestenum.py
+++ contrib/grinder/scripts/ingestenum.py
@@ -90,7 +90,7 @@ class EnumIngestThread(AbstractThread):
                                         self.slice[self.position])
         self.position += 1
         headers = ( NVPair("Content-Type", "application/json"), )
-        result = self.request.POST(self.ingest_url(tenant_id), payload, headers)
+        result = self.request.POST(self.ingest_url(), payload, headers)
         if result.getStatusCode() in [400, 415, 500]:
             logger("Error: status code=" + str(result.getStatusCode()) + " response=" + result.getText())
         return result
