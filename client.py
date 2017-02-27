1c1718c768b36e0568ae30e90e75bbd6250d5a5e
Richard Sartor <richard.sartor@rackspace.com>
2017-02-27 17:10:27 -0600

    Add cli commands for single-plot and multi-plot queries.

--- client.py
+++ client.py
@@ -97,6 +97,66 @@ def make_search_query_request(base_url, token, tenant, query):
     return success
 
 
+def make_single_plot_request(base_url, token, tenant, metric_name, from_, to,
+                             resolution=None):
+    if resolution is None:
+        resolution = 'FULL'
+
+    url = "{}/v2.0/{}/views/{}?from={}&to={}&resolution={}".format(
+        base_url, tenant, metric_name, from_, to, resolution)
+
+    request = requests.Request('GET', url)
+    if token:
+        request.headers['X-Auth-Token'] = token
+    preq = request.prepare()
+
+    if debug:
+        print_request(preq)
+
+    session = requests.session()
+    response = session.send(preq)
+
+    if debug:
+        print('')
+        print_response(response)
+
+    success = 200 <= response.status_code < 300
+    print(response.text)
+    return success
+
+
+def make_multi_plot_request(base_url, token, tenant, metric_names, from_, to,
+                            resolution=None):
+    if resolution is None:
+        resolution = 'FULL'
+
+    url = "{}/v2.0/{}/views?from={}&to={}&resolution={}".format(
+        base_url, tenant, from_, to, resolution)
+
+    payload = list(metric_names)
+
+    request = requests.Request('POST', url, json=payload)
+    request.headers['Content-Type'] = 'application/json'
+    if token:
+        request.headers['X-Auth-Token'] = token
+
+    preq = request.prepare()
+
+    if debug:
+        print_request(preq)
+
+    session = requests.session()
+    response = session.send(preq)
+
+    if debug:
+        print('')
+        print_response(response)
+
+    success = 200 <= response.status_code < 300
+    print(response.text)
+    return success
+
+
 def main():
 
     parser = argparse.ArgumentParser()
@@ -113,6 +173,8 @@ def main():
     subs = parser.add_subparsers(help='subparsers?', dest='command')
 
     BF_INGEST_URL = environ.get('BF_INGEST_URL', None)
+    ingest_url_help = 'The endpoint to send HTTP ingest requests to. ' \
+                      'Defaults to the value of the BF_INGEST_URL envvar.'
 
     ingest_sub = subs.add_parser('ingest', help='Send metrics to blueflood.')
     ingest_sub.add_argument('tenant')
@@ -124,21 +186,39 @@ def main():
     ingest_sub.add_argument('--ttl-seconds', type=int, default=172800)
     ingest_sub.add_argument('--collection-time')
     ingest_sub.add_argument('--url', type=str, action='store',
-                            default=BF_INGEST_URL,
-                            help='The endpoint to send HTTP ingest requests '
-                                 'to. Defaults to the value of the '
-                                 'BF_INGEST_URL envvar.')
+                            default=BF_INGEST_URL, help=ingest_url_help)
 
     BF_QUERY_URL = environ.get('BF_QUERY_URL', None)
+    query_url_help = 'The endpoint to send HTTP query requests to. Defaults ' \
+                     'to the value of the BF_QUERY_URL envvar.'
 
     search_sub = subs.add_parser('search', help='Search for things.')
     search_sub.add_argument('tenant')
     search_sub.add_argument('query')
     search_sub.add_argument('--url', type=str, action='store',
-                            default=BF_QUERY_URL,
-                            help='The endpoint to send HTTP query requests to. '
-                                 'Defaults to the value of the BF_QUERY_URL '
-                                 'envvar.')
+                            default=BF_QUERY_URL, help=query_url_help)
+
+    single_plot_sub = subs.add_parser('single-plot', help='Query for things')
+    single_plot_sub.add_argument('tenant')
+    single_plot_sub.add_argument(metavar='metric-name', dest='metric_name')
+    single_plot_sub.add_argument(metavar='from-milliseconds',
+                                 dest='from_milliseconds', type=int)
+    single_plot_sub.add_argument(metavar='to-milliseconds',
+                                 dest='to_milliseconds', type=int)
+    single_plot_sub.add_argument('--resolution', choices=['FULL'])
+    single_plot_sub.add_argument('--url', type=str, action='store',
+                                 default=BF_QUERY_URL, help=query_url_help)
+
+    multi_plot_sub = subs.add_parser('multi-plot', help='Query for things')
+    multi_plot_sub.add_argument('tenant')
+    multi_plot_sub.add_argument(metavar='metric-name', dest='metric_name')
+    multi_plot_sub.add_argument(metavar='from-milliseconds',
+                                dest='from_milliseconds', type=int)
+    multi_plot_sub.add_argument(metavar='to-milliseconds',
+                                dest='to_milliseconds', type=int)
+    multi_plot_sub.add_argument('--resolution', choices=['FULL'])
+    multi_plot_sub.add_argument('--url', type=str, action='store',
+                                default=BF_QUERY_URL, help=query_url_help)
 
     args = parser.parse_args()
 
@@ -163,6 +243,18 @@ def main():
     elif args.command == 'search':
         success = make_search_query_request(base_url, args.token, args.tenant,
                                             args.query)
+    elif args.command == 'single-plot':
+        success = make_single_plot_request(base_url, args.token, args.tenant,
+                                           args.metric_name,
+                                           args.from_milliseconds,
+                                           args.to_milliseconds,
+                                           args.resolution)
+    elif args.command == 'multi-plot':
+        success = make_multi_plot_request(base_url, args.token, args.tenant,
+                                          [args.metric_name],
+                                          args.from_milliseconds,
+                                          args.to_milliseconds,
+                                          args.resolution)
     else:
         print('Unknown command "{}"'.format(args.command))
         exit(1)
